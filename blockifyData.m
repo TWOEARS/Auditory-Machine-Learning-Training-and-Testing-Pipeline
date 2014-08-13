a7368ae86794b23856f012c8b9b5a9b41f7c4771
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-08-14 00:48:02 +0200

    adapted blockifyData to oop-wp2

--- blockifyData.m
+++ blockifyData.m
@@ -11,38 +11,45 @@ for z = 1:length( soundFileNames )
     
     blocksSaveName = [soundFileNames{z} '.' blockDataHash '.blocks.mat'];
     if exist( blocksSaveName, 'file' ); continue; end;
-
+    
     wp2SaveName = [soundFileNames{z} '.' getWp2dataHash( esetup ) '.wp2.mat'];
     ls = load( wp2SaveName, 'wp2data' );
     wp2data = ls.wp2data;
     
     wp2BlockFeatures = [];
     
-    for k = 1:size( wp2data, 1 ) % different wp2cues/features
+    for k = 1:size( wp2data, 1 ) % different wp2 requests
         wp2BlockFeaturesTmp = [];
         for m = 1:size( wp2data, 2 ) % different earsignals (e.g. different angles)
             
             fprintf( '.' );
             
-            nHops = size( wp2data(k,m).data, 2 );
-            bs = getBlockSizes( esetup );
-            nHopsMinusLastBlock = max( nHops - bs.hopsPerBlock, 0 );
-            for blockIdx = 1:(1 + ceil( nHopsMinusLastBlock / bs.hopsPerShift ) )
-                
-                blockstart = 1 + (blockIdx - 1) * bs.hopsPerShift;
-                blockend = min( blockstart + bs.hopsPerBlock - 1, nHops );
-                if (blockend - blockstart + 1) < bs.hopsPerBlock
-                    blockstart = nHopsMinusLastBlock + 1;
+            sigLen = size( wp2data{k,m}{1}.Data, 1 );
+            [blockLen,shiftLen] = getBlockSizes( esetup, wp2data{k,m}{1} );
+            sigLenMinusLastBlock = max( sigLen - blockLen, 0 );
+            nBlocks = (1 + ceil( sigLenMinusLastBlock / shiftLen ) );
+            for bi = 1:nBlocks
+                blockstart = 1 + (bi - 1) * shiftLen;
+                blockend = min( blockstart + blockLen - 1, sigLen );
+                if (blockend - blockstart + 1) < blockLen
+                    blockstart = sigLenMinusLastBlock + 1;
                 end
-                block = wp2data(k,m);
-                block.data = block.data(:,blockstart:blockend,:);
-                block.startTime = (blockstart - 1) * esetup.wp2dataCreation.hopSizeSec;
-                block.endTime = (blockend - 1) * esetup.wp2dataCreation.hopSizeSec + esetup.wp2dataCreation.winSizeSec;
+                block = [];
+                block.Data{1} = wp2data{k,m}{1}.Data(blockstart:blockend,:);
+                block.Data{2} = wp2data{k,m}{2}.Data(blockstart:blockend,:);
+                block.startTime = (blockstart - 1) / wp2data{k,m}{1}.FsHz;
+                block.endTime = blockend / wp2data{k,m}{1}.FsHz; % incorrect. See below
+                % endTime = (blockend - 1) / wp2data{k,m}{1}.FsHz + esetup.wp2dataCreation.winSizeSec;
+                block.Name = wp2data{k,m}{1}.Name;
+                block.Dimensions = wp2data{k,m}{1}.Dimensions;
+                block.FsHz = wp2data{k,m}{1}.FsHz;
+                block.Canal{1} = wp2data{k,m}{1}.Canal;
+                block.Canal{2} = wp2data{k,m}{2}.Canal;
                 
-                wp2BlockFeaturesTmp = [wp2BlockFeaturesTmp;block];
+                wp2BlockFeaturesTmp = [wp2BlockFeaturesTmp block];
             end
         end
-        wp2BlockFeatures = [wp2BlockFeatures wp2BlockFeaturesTmp];
+        wp2BlockFeatures = [wp2BlockFeatures; wp2BlockFeaturesTmp];
     end
     
     fprintf( '.' );
--- getBlockSizes.m
+++ getBlockSizes.m
@@ -1,11 +1,6 @@
-function blockSizes = getBlockSizes( esetup )
+function [blockLen,shiftLen] = getBlockSizes( esetup, signal )
 
-blockSizes.winSamples = 2 * round( esetup.wp2dataCreation.winSizeSec * esetup.wp2dataCreation.fsHz / 2 );
-blockSizes.hopSamples = 2 * round( esetup.wp2dataCreation.hopSizeSec * esetup.wp2dataCreation.fsHz / 2 );
-blockSizes.blockSamples = esetup.wp2dataCreation.fsHz * esetup.blockCreation.blockSize;
-blockSizes.shiftSamples = esetup.wp2dataCreation.fsHz * esetup.blockCreation.shiftSize;
-
-blockSizes.hopsPerBlock = 2 * round( 0.5 * esetup.blockCreation.blockSize / esetup.wp2dataCreation.hopSizeSec ) - 1; % -1 because otherwise the last frame of a block would last until past the end of that block
-blockSizes.hopsPerShift  = 2 * round( 0.5 * esetup.blockCreation.shiftSize / esetup.wp2dataCreation.hopSizeSec );
+blockLen = 2 * round( 0.5 * esetup.blockCreation.blockSize * signal.FsHz ) - 1; % -1 because otherwise the last frame of a block would last until past the end of that block
+shiftLen = 2 * round( 0.5 * esetup.blockCreation.shiftSize * signal.FsHz );
 
 

02e4e2426c276c945f204e0d2689fe923584e164
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-08-14 01:53:18 +0200

    adapt feature extraction to switched dimensions + L/R cell

--- makeFeaturesFromWp2Blocks.m
+++ makeFeaturesFromWp2Blocks.m
@@ -1,9 +1,9 @@
 function features = makeFeaturesFromWp2Blocks( wp2Data, esetup )
  
-nBlocks = size( wp2Data, 1 );
+nBlocks = size( wp2Data, 2 );
 for blockIdx = 1:nBlocks 
     fprintf( '.' );
-    blockFeatures = esetup.featureCreation.function( esetup.featureCreation.functionParam, wp2Data(blockIdx,:) );
+    blockFeatures = esetup.featureCreation.function( esetup.featureCreation.functionParam, wp2Data(:,blockIdx) );
     if blockIdx == 1
         lenFeatureVector = length( blockFeatures );
         features = zeros( nBlocks, lenFeatureVector );
--- msFeatures.m
+++ msFeatures.m
@@ -3,21 +3,19 @@ function features = msFeatures( param, wp2Data )
 % of one. Reduces each freq channel to its mean and std + mean and std of
 % finite differences.
 
-rmBlock = wp2Data(strcmp('ratemap_magnitude', vertcat(wp2Data.name))).data;
-rmBlock = rmBlock.^0.33; %cuberoot compression
-for ii = 1 : size(rmBlock,3)
-    scale = max(max(abs(rmBlock(:,:,ii))));
-    if any(scale==0) || any(~isfinite(scale))
-        rmBlock(:,:,ii) = rmBlock(:,:,ii);
-    else
-        rmBlock(:,:,ii) = rmBlock(:,:,ii)./repmat(scale,size(rmBlock(:,:,ii)));
+rmBlock = wp2Data(strcmp('ratemap_magnitude', vertcat(wp2Data.Name))).Data;
+for ci = 1 : size(rmBlock,2)
+    rmBlock{ci} = rmBlock{ci}.^0.33; %cuberoot compression
+    scale = max(max(abs(rmBlock{ci})));
+    if ~( any(scale==0) || any(~isfinite(scale)) )
+        rmBlock{ci} = rmBlock{ci}./repmat(scale,size(rmBlock{ci}));
     end
 end
 
 % TODO: determine which channel to use!
-s = .5 * rmBlock(:,:,1) + .5 * rmBlock(:,:,2);
-features = [mean( s, 2 ); std( s, 0, 2 )];
+s = .5 * rmBlock{1} + .5 * rmBlock{2};
+features = [mean( s, 1 )  std( s, 0, 1 )];
 for i = 1:param.derivations
-    s = s(:,2:end) - s(:,1:end-1);
-    features = [features; mean( s, 2 ); std( s, 0, 2 )];
+    s = s(2:end,:) - s(1:end-1,:);
+    features = [features  mean( s, 1 )  std( s, 0, 1 )];
 end

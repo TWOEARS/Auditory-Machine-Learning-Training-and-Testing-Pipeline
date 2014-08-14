0a3b9bde5f1420fe1c4f6aa22db9b2382ff4e5c1
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-08-14 02:45:44 +0200

    added BACext to validation_function

--- validation_function.m
+++ validation_function.m
@@ -1,6 +1,6 @@
-function ret = validation_function(pred, labels);
+function ret = validation_function(pred, labels)
 labels = (labels >= 0) - (labels < 0);
-valid_function = @(dec, labels) bac(dec, labels);
+valid_function = @(dec, labels) bacExt(dec, labels);
 ret = valid_function(pred, labels);
 %precision(dec, labels);
 %recall(dec, labels);
@@ -95,3 +95,22 @@ function ret = accuracy(dec, label)
   ret = correct / total;
   disp(sprintf('Accuracy = %g%% (%d/%d)', 100.0 * ret, correct, total));
 
+function ret = bacExt(dec, label)
+tp = sum(label == 1 & dec >= 0);
+tn = sum(label == -1 & dec < 0);
+tp_fn = sum(label == 1);
+tn_fp = sum(label == -1);
+if tp_fn == 0;
+  disp(sprintf('warning: No positive true label.'));
+  sensitivity = 0;
+else
+  sensitivity = tp / tp_fn;
+end
+if tn_fp == 0;
+  disp(sprintf('warning: No negative true label.'));
+  specificity = 0;
+else
+  specificity = tn / tn_fp;
+end
+ret = 1 - (((1-sensitivity)^2 + (1-specificity)^2) / 2)^0.5;
+disp(sprintf('BACext = %g (Sens = %g, Spec = %g)', ret, sensitivity, specificity));

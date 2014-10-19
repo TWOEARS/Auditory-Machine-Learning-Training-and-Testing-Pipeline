267b9f9532e4174eae9bc5cb0062f11aa7535556
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-10-19 21:16:16 +0200

    added Hashable.m

--- Hashable.m
+++ Hashable.m
@@ -1,26 +1,46 @@
-classdef Hashable < handle
-
+classdef (HandleCompatible) Hashable    
+    % Interface for classes that provide a method to encode their status as 
+    % a hashcode. 
+    % Overwrite getHashObjects to leave out properties of hash calculation.
+    % Can be superclass of handle or value class
+    
     %%---------------------------------------------------------------------
-    properties (SetAccess = private)
+    properties 
     end
     
     %%---------------------------------------------------------------------
-    methods (Static)
-    end
-    
-    %%---------------------------------------------------------------------
-    methods (Access = public)
+    methods
         
-        function hash = getHash( obj, maxRecursionLevel )
-            if ~exist( 'maxRecursionLevel', 'var' ), maxRecursionLevel = 10; end;
-            hash = DataHash( obj, maxRecursionLevel );
+        function hashcode = getHash( obj )
+            hashMembers = obj.getHashObjects();
+            hashcode = DataHash( hashMembers );
+        end
+        
+        function hashMembers = getHashObjects( obj )
+            mcdata = metaclass( obj );
+            propsData = mcdata.PropertyList;
+            warning off MATLAB:structOnObject
+            propsStruct = struct( obj );
+            warning on MATLAB:structOnObject
+            hashMembers = {};
+            for p = propsData'
+                if p.Transient, continue; end
+                if ~isfield( propsStruct, p.Name ), continue; end
+                hashMembers{end+1} = propsStruct.(p.Name);
+            end
         end
         
     end
     
     %%---------------------------------------------------------------------
-    methods (Access = private)
+    methods (Abstract)
+        
+        
     end
+    
+    %%---------------------------------------------------------------------
+    
+
     
     %%---------------------------------------------------------------------
     

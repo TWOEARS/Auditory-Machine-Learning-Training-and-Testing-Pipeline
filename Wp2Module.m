96ee5b30030c018c9600e03d993e960bd3fdd7ad
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-10-20 20:04:23 +0200

    Wp2Module.m: use Map instead of cell array for outputSignals

--- Wp2Module.m
+++ Wp2Module.m
@@ -16,6 +16,7 @@ classdef Wp2Module < IdWp2ProcInterface
         
         function obj = Wp2Module()
             obj = obj@IdWp2ProcInterface();
+            obj.outputSignals = containers.Map( 'KeyType', 'char', 'ValueType', 'any' );
         end
         
         function hashMembers = getHashObjects( obj )
@@ -28,7 +29,7 @@ classdef Wp2Module < IdWp2ProcInterface
             obj.wp2dataObj = dataObject( [], fs, 2, 1 );
             obj.managerObject = manager( obj.wp2dataObj );
             for ii = 1:length( wp2Requests )
-                obj.outputSignals{ii} = obj.managerObject.addProcessor( ...
+                obj.outputSignals(wp2Requests{ii}.name) = obj.managerObject.addProcessor( ...
                     wp2Requests{ii}.name, wp2Requests{ii}.params );
             end
         end
@@ -42,13 +43,12 @@ classdef Wp2Module < IdWp2ProcInterface
             obj.managerObject.reset();
             obj.wp2dataObj.clearData();
             fs = obj.wp2dataObj.signal{1,1}.FsHz;
-            for ii = 1:numel( obj.outputSignals )
-                outputSig = obj.outputSignals{ii};
-                for kk = 1:numel( outputSig )
-                    if isa( outputSig, 'cell' )
-                        os = outputSig{kk}; 
+            for outputSig = obj.outputSignals.values
+                for kk = 1:numel( outputSig{1} )
+                    if isa( outputSig{1}, 'cell' )
+                        os = outputSig{1}{kk}; 
                     else
-                        os = outputSig(kk);
+                        os = outputSig{1}(kk);
                     end
                     os.setBufferSize( ceil( length( earSignals ) / fs ) );
                 end

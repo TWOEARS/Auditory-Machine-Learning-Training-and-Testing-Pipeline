8a1038e488fc0552cf36d200ce39050bbc2f91da
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2014-10-27 21:29:32 +0100

    FeatureSet1Blockmean.m: use l-moments and use different freq channel numbers for different features

--- FeatureSet1Blockmean.m
+++ FeatureSet1Blockmean.m
@@ -6,7 +6,10 @@ classdef FeatureSet1Blockmean < FeatureProcInterface
     %% --------------------------------------------------------------------
     properties (SetAccess = private)
         freqChannels;
+        freqChannelsStatistics;
+        amFreqChannels;
         deltasLevels;
+        amChannels;
     end
     
     %% --------------------------------------------------------------------
@@ -19,15 +22,19 @@ classdef FeatureSet1Blockmean < FeatureProcInterface
         function obj = FeatureSet1Blockmean( )
             obj = obj@FeatureProcInterface();
             obj.freqChannels = 16;
+            obj.amFreqChannels = 8;
+            obj.freqChannelsStatistics = 32;
             obj.deltasLevels = 1;
+            obj.amChannels = 4;
         end
         %% ----------------------------------------------------------------
 
         function afeRequests = getAFErequests( obj )
             afeRequests{1}.name = 'modulation';
             afeRequests{1}.params = genParStruct( ...
-                'nChannels', obj.freqChannels, ...
-                'am_type', 'filter' ...
+                'nChannels', obj.amFreqChannels, ...
+                'am_type', 'filter', ...
+                'am_nFilters', obj.amChannels ...
                 );
             afeRequests{2}.name = 'ratemap_magnitude';
             afeRequests{2}.params = genParStruct( ...
@@ -35,7 +42,7 @@ classdef FeatureSet1Blockmean < FeatureProcInterface
                 );
             afeRequests{3}.name = 'spec_features';
             afeRequests{3}.params = genParStruct( ...
-                'nChannels', obj.freqChannels ...
+                'nChannels', obj.freqChannelsStatistics ...
                 );
             afeRequests{4}.name = 'onset_strength';
             afeRequests{4}.params = genParStruct( ...
@@ -50,34 +57,37 @@ classdef FeatureSet1Blockmean < FeatureProcInterface
             rmL = compressAndScale( rmRL{2}.Data, 0.33, @(x)(median( x(x>0.01) )), 0 );
             rm = 0.5 * rmR + 0.5 * rmL;
             spfRL = afeData('spec_features');
-            spfR = compressAndScale( spfRL{1}.Data, 0.33, @(x)(median( abs(x(abs(x)>0.01)) )), 2 );
-            spfL = compressAndScale( spfRL{2}.Data, 0.33, @(x)(median( abs(x(abs(x)>0.01)) )), 2 );
+            spfR = compressAndScale( spfRL{1}.Data, 0.33, @(x)(median( abs(x(abs(x)>0.01)) )), 1 );
+            spfL = compressAndScale( spfRL{2}.Data, 0.33, @(x)(median( abs(x(abs(x)>0.01)) )), 1 );
             spf = 0.5 * spfL + 0.5 * spfR;
             onsRL = afeData('onset_strength');
             onsR = compressAndScale( onsRL{1}.Data, 0.33, @(x)(median( x(x>0.01) )), 0 );
             onsL = compressAndScale( onsRL{2}.Data, 0.33, @(x)(median( x(x>0.01) )), 0 );
             ons = 0.5 * onsR + 0.5 * onsL;
             xBlock = [rm, spf, ons];
-            x = [mean( xBlock, 1 )  std( xBlock, 0, 1 )];
+            x = lMomentAlongDim( xBlock, 4, 1 );
             for i = 1:obj.deltasLevels
                 xBlock = xBlock(2:end,:) - xBlock(1:end-1,:);
-                x = [x  mean( xBlock, 1 )  std( xBlock, 0, 1 )];
+                x = [x  lMomentAlongDim( xBlock, 4, 1 )];
             end
             modRL = afeData('modulation');
             modR = compressAndScale( modRL{1}.Data, 0.33, @(x)(median( x(x>0.01) )), 0 );
             modL = compressAndScale( modRL{2}.Data, 0.33, @(x)(median( x(x>0.01) )), 0 );
             mod = 0.5 * modR + 0.5 * modL;
             mod = reshape( mod, size( mod, 1 ), size( mod, 2 ) * size( mod, 3 ) );
-            x = [x mean( mod, 1 )  std( mod, 0, 1 )];
+            x = [x lMomentAlongDim( mod, 4, 1 )];
             for i = 1:obj.deltasLevels
                 mod = mod(2:end,:) - mod(1:end-1,:);
-                x = [x  mean( mod, 1 )  std( mod, 0, 1 )];
+                x = [x lMomentAlongDim( mod, 4, 1 )];
             end
         end
         %% ----------------------------------------------------------------
         
         function outputDeps = getInternOutputDependencies( obj )
             outputDeps.freqChannels = obj.freqChannels;
+            outputDeps.amFreqChannels = obj.amFreqChannels;
+            outputDeps.freqChannelsStatistics = obj.freqChannelsStatistics;
+            outputDeps.amChannels = obj.amChannels;
             outputDeps.deltasLevels = obj.deltasLevels;
             classInfo = metaclass( obj );
             classname = classInfo.Name;

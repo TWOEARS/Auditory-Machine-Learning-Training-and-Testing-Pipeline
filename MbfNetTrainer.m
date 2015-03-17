21d81abad14e6960d1667381ed5d9cd8ae471873
Jalil <taghia@ni.tu-berlin.de>
2015-03-17 18:12:52 +0100

    added a working MBFmodelSelectTrainer.m

--- MbfNetTrainer.m
+++ MbfNetTrainer.m
@@ -1,4 +1,4 @@
-classdef vMFTrainer < IdTrainerInterface & Parameterized
+classdef MbfNetTrainer < IdTrainerInterface & Parameterized
     
     %% --------------------------------------------------------------------
     properties (Access = protected)
@@ -8,14 +8,20 @@ classdef vMFTrainer < IdTrainerInterface & Parameterized
     %% --------------------------------------------------------------------
     methods
 
-        function obj = vMFTrainer( varargin )
+        function obj = MbfNetTrainer( varargin )
             pds{1} = struct( 'name', 'performanceMeasure', ...
-                             'default', @BAC2, ...
-                             'valFun', @(x)(isa( x, 'function_handle' )), ...
-                             'setCallback', @(ob, n, o)(ob.setPerformanceMeasure( n )) );
-            pds{2} = struct( 'name', 'maxDataSize', ...
-                             'default', inf, ...
-                             'valFun', @(x)(isinf(x) || (rem(x,1) == 0 && x > 0)) );
+                'default', @BAC2, ...
+                'valFun', @(x)(isa( x, 'function_handle' )), ...
+                'setCallback', @(ob, n, o)(ob.setPerformanceMeasure( n )) );
+            pds{2} = struct( 'name', 'nComp', ...
+                'default', [1 2 3], ...
+                'valFun', @(x)(sum(x)>=0) );
+            pds{3} = struct( 'name', 'thr', ...
+                             'default', [0.5 0.6], ...
+                             'valFun', @(x)(x<=1 && x >= 0) );
+            pds{4} = struct( 'name', 'maxDataSize', ...
+                'default', inf, ...
+                'valFun', @(x)(isinf(x) || (rem(x,1) == 0 && x > 0)) );
             obj = obj@Parameterized( pds );
             obj.setParameters( true, varargin{:} );
         end
@@ -27,23 +33,20 @@ classdef vMFTrainer < IdTrainerInterface & Parameterized
                 x(obj.parameters.maxDataSize+1:end,:) = [];
                 y(obj.parameters.maxDataSize+1:end) = [];
             end
-            obj.model = vMFModel();
+            obj.model = MbfNetModel();
             xScaled = obj.model.scale2zeroMeanUnitVar( x, 'saveScalingFactors' );
-%             glmOpts.alpha = obj.parameters.alpha;
-%             glmOpts.nlambda = obj.parameters.nLambda;
-%             if ~isempty( obj.parameters.lambda )
-%                 glmOpts.lambda = obj.parameters.lambda;
-%             end
-%             verboseFprintf( obj, 'GlmNet training with alpha=%f\n', glmOpts.alpha );
-%             verboseFprintf( obj, '\tsize(x) = %dx%d\n', size(x,1), size(x,2) );
+            mbfOpts.nComp = obj.parameters.nComp;
+            mbfOpts.thr = obj.parameters.thr;
+            if ~isempty( obj.parameters.nComp )
+                mbfOpts.nComp = obj.parameters.nComp;
+            end
+            verboseFprintf( obj, 'MbfNet training with nComp=%f and thr=%f\n', mbfOpts.nComp, mbfOpts.thr);
+            verboseFprintf( obj, '\tsize(x) = %dx%d\n', size(x,1), size(x,2) );
 %             obj.model.model = glmnet( xScaled, y, obj.parameters.family, glmOpts );
-            gmmOpts.initComps = 4;
-            idFeature = featureSelectionPCA2(xScaled,.4);
-            xScaled = xScaled(:,idFeature);
-            xTrain = normvec(xScaled');
-            [obj.model.model{1}, obj.model.model{2}] = trainVMF( y, xTrain', gmmOpts );
-            obj.model.model{3}=idFeature;
-            % train +1 model
+            mbfOpts.initComps = mbfOpts.nComp;
+          idFeature = featureSelectionPCA2(xScaled,mbfOpts.thr);
+            [obj.model.model{1}, obj.model.model{2}] = trainMbfs( y, xScaled(:,idFeature), mbfOpts );
+            obj.model.model{3}=idFeature;            % train +1 model
             % call obj.setPositiveClass( 'general' );
             verboseFprintf( obj, '\n' );
         end

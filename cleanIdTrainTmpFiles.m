0e09d70e1e3a38b9a7075403c5c30f6b1adef987
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-07-01 16:55:14 +0200

    cleanIdTrainTmpFiles: add functionality to delete ranges and complete types of proc tmp files

--- cleanIdTrainTmpFiles.m
+++ cleanIdTrainTmpFiles.m
@@ -14,26 +14,76 @@ while true
     ii = 1;
     while ii <= length( procFoldersDir )
         if exist( [procFoldersDir(ii).name filesep 'config.mat'], 'file' )
-            fprintf( '%i: \t%s\n', ii, procFoldersDir(ii).name );
             ii = ii + 1;
         else
             procFoldersDir(ii) = [];
         end
     end
     
-    choice = input( ['\n''stfu'' to quit. Enter to look through all,' ...
-        'or choose the according number and then press enter. >>'] );
+    procList = listProcFolders( procFoldersDir );
+
+    choice = [];
+    while isempty( choice )
+        choice = input( ['\n''q'' to quit. ' ...
+                         '''l'' nr to look, '...
+                         '''d'' nr to delete.'...
+                         'nr can be a range as in 10-50. >> '], 's' );
+    end
     
-    if ischar( choice ) && strcmpi( choice, 'q' )
+    if strcmpi( choice, 'q' )
         break;
-    elseif isempty( choice ) % direct enter
-        for ii = 1 : length( procFoldersDir )
-            presentProcFolder( procFoldersDir(ii).name );
-        end
     else
-        presentProcFolder( procFoldersDir(choice).name );
+        [cmd,arg] = strtok( choice, ' ' );
+        listNames = keys(procList);
+        [arg1,arg2] = strtok( arg, '-' );
+        if isempty( arg2 ), arg2 = arg1; end
+        arg = str2double( arg1 ) : str2double( arg2(2:end) );
+        idxs = [];
+        for ii = 1 : numel( arg )
+            idxs = [idxs procList(listNames{arg(ii)})];
+        end
+        if strcmpi( cmd, 'l' )
+            for ii = idxs
+                presentProcFolder( procFoldersDir(ii).name );
+                input( 'press enter to continue', 's' );
+            end
+        elseif strcmpi( cmd, 'd' )
+            for ii = idxs
+                fprintf( 'Deleting %s...\n', procFoldersDir(ii).name );
+                rmdir( procFoldersDir(ii).name, 's' );
+            end
+        end
     end
 end
+
+end
+
+% ---------------------------------------------------------------------------------------%
+
+function procList = listProcFolders( procFolders )
+
+choice = input( 'Enter to see all folders, ''t'' to see by type, ''c'' by content >> ', 's' );
+procList = containers.Map('KeyType','char','ValueType','any');
+if isempty( choice )
+    for ii = 1 : length( procFolders )
+        procList(procFolders(ii).name) = ii;
+    end
+elseif strcmpi( choice, 't' )
+    for ii = 1 : length( procFolders )
+        procName = strtok( procFolders(ii).name, '.' );
+        if ~procList.isKey( procName )
+            procList(procName) = ii;
+        else
+            procList(procName) =  [procList(procName), ii];
+        end
+    end
+elseif strcmpi( choice, 'c' )
+end
+procNames = keys( procList );
+for ii = 1 : length( procNames )
+    fprintf( '%i: \t%s\n', ii, procNames{ii} );
+end
+
 
 end
 
@@ -44,20 +94,8 @@ function presentProcFolder( procFolder )
 cprintf( '-Blue', '\n.:%s:.\n', procFolder );
 config = load( [procFolder filesep 'config.mat'] );
 flatPrintObject( config, 10 );
-choice = input( 'Enter to do nothing, ''d'' to delete this tmp proc folder. >>', 's' );
-if ~isempty( choice ) && strcmpi( choice, 'd' )
-    fprintf( 'Deleting %s...\n', procFolder );
-    rmdir( procFolder, 's' );
-end
 
 end
 
 % ---------------------------------------------------------------------------------------%
 
-function choice = stfu()
-
-choice = 'q';
-
-end
-
-% ---------------------------------------------------------------------------------------%

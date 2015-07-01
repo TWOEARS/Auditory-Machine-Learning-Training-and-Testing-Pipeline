c00fc2e5ea4d3eb0b2f81d658d57df741c0fe37b
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-07-01 18:26:09 +0200

    cleanIdTrainTmpFiles: added 'contents' option for choosing folders

--- cleanIdTrainTmpFiles.m
+++ cleanIdTrainTmpFiles.m
@@ -40,7 +40,7 @@ while true
         arg = str2double( arg1 ) : str2double( arg2(2:end) );
         idxs = [];
         for ii = 1 : numel( arg )
-            idxs = [idxs procList(listNames{arg(ii)})];
+            idxs = [idxs getMapStructElem( procList, listNames{arg(ii)}, 'idxs' )];
         end
         if strcmpi( cmd, 'l' )
             for ii = idxs
@@ -66,22 +66,46 @@ choice = input( 'Enter to see all folders, ''t'' to see by type, ''c'' by conten
 procList = containers.Map('KeyType','char','ValueType','any');
 if isempty( choice )
     for ii = 1 : length( procFolders )
-        procList(procFolders(ii).name) = ii;
+        assignMapStructElem( procList, procFolders(ii).name, 'idxs', ii );
     end
 elseif strcmpi( choice, 't' )
     for ii = 1 : length( procFolders )
         procName = strtok( procFolders(ii).name, '.' );
         if ~procList.isKey( procName )
-            procList(procName) = ii;
+            assignMapStructElem( procList, procName, 'idxs', ii );
         else
-            procList(procName) =  [procList(procName), ii];
+            assignMapStructElem( procList, procName, 'idxs', ...
+                [getMapStructElem( procList, procName, 'idxs' ) ii] );
         end
     end
 elseif strcmpi( choice, 'c' )
+    for ii = 1 : length( procFolders )
+        iiConfig = load( [procFolders(ii).name filesep 'config.mat'] );
+        if isempty( procList )
+            assignMapStructElem( procList, procFolders(ii).name, 'idxs', ii );
+            assignMapStructElem( procList, procFolders(ii).name, 'config', iiConfig );
+        else
+            procNames = keys( procList );
+            configFound = false;
+            for jj = 1 : length( procNames )
+                if isequalDeepCompare( getMapStructElem( procList, procNames{jj}, 'config' ), iiConfig )
+                    assignMapStructElem( procList, procNames{jj}, 'idxs',...
+                        [getMapStructElem( procList, procNames{jj}, 'idxs' ), ii] );
+                    configFound = true;
+                    break;
+                end
+            end
+            if ~configFound
+                assignMapStructElem( procList, procFolders(ii).name, 'idxs', ii );
+                assignMapStructElem( procList, procFolders(ii).name, 'config', iiConfig );
+            end
+        end
+    end
 end
 procNames = keys( procList );
 for ii = 1 : length( procNames )
-    fprintf( '%i: \t%s\n', ii, procNames{ii} );
+    fprintf( '%i: \t%s \t(%i folders)\n', ...
+        ii, procNames{ii}, numel( getMapStructElem( procList, procNames{ii}, 'idxs' ) ) );
 end
 
 
@@ -94,6 +118,25 @@ function presentProcFolder( procFolder )
 cprintf( '-Blue', '\n.:%s:.\n', procFolder );
 config = load( [procFolder filesep 'config.mat'] );
 flatPrintObject( config, 10 );
+
+end
+
+% ---------------------------------------------------------------------------------------%
+
+function assignMapStructElem( map, key, fieldname, val )
+
+if map.isKey( key )
+    s = map(key);
+end
+s.(fieldname) = val;
+map(key) = s;
+
+end
+
+function val = getMapStructElem( map, key, fieldname  )
+
+s = map(key);
+val = s.(fieldname);
 
 end
 

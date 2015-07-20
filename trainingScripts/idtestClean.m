41c95c569d559a7830e8fd41986cda3e09f532ac
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-07-20 18:14:28 +0200

    * created trainingScripts in identificationTraining -> folder for (still generic) scripts like idtrainClean.m, idtestClean.m

---
+++ trainingScripts/idtestClean.m
@@ -0,0 +1,19 @@
+function idtestClean( classname, testFlist, modelPath )
+
+testpipe = TwoEarsIdTrainPipe();
+m = load( fullfile( modelPath, [classname '.model.mat'] ) );
+testpipe.featureCreator = m.featureCreator;
+testpipe.modelCreator = ...
+    modelTrainers.LoadModelNoopTrainer( ...
+        @(cn)(fullfile( modelPath, [cn '.model.mat'] )), ...
+        'performanceMeasure', @performanceMeasures.BAC2 ...
+        );
+testpipe.modelCreator.verbose( 'on' );
+testpipe.testset = testFlist;
+sc = dataProcs.SceneConfiguration(); % clean
+testpipe.setSceneConfig( [sc] ); 
+testpipe.init();
+
+testpipe.pipeline.run( {classname}, 0 );
+
+end

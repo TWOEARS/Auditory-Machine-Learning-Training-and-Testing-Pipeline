56e139e05ca6945b7f6ddd002ce31e485acddab9
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2015-08-03 16:13:42 +0200

    Add missing folders to start script

--- startIdentificationTraining.m
+++ startIdentificationTraining.m
@@ -6,5 +6,14 @@ basePath = fileparts(mfilename('fullpath'));
 
 % Add all relevant folders to the matlab search path
 addpath(fullfile(basePath, 'src'));
+addpath(fullfile(basePath, 'src', '+core'));
+addpath(fullfile(basePath, 'src', '+dataProcs'));
+addpath(fullfile(basePath, 'src', 'evaluation'));
+addpath(fullfile(basePath, 'src', '+featureCreators'));
+addpath(fullfile(basePath, 'src', '+models'));
+addpath(fullfile(basePath, 'src', '+modelTrainers'));
+addpath(fullfile(basePath, 'src', '+performanceMeasures'));
+addpath(fullfile(basePath, 'src', 'postpros'));
+addpath(fullfile(basePath, 'src', 'trainingScripts'));
 
 clear basePath;

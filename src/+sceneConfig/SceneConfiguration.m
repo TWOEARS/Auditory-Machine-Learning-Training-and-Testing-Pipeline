46a93fcde2f311b7e2a414704ffd8d8d8cad9169
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-09-05 01:41:18 +0200

    SceneConfiguration.m reworked, using new source system

--- src/+sceneConfig/SceneConfiguration.m
+++ src/+sceneConfig/SceneConfiguration.m
@@ -1,16 +1,11 @@
-classdef SceneConfiguration < handle
+classdef SceneConfiguration < matlab.mixin.Copyable
 
     %% -----------------------------------------------------------------------------------
-    properties
-        angleSignal;
-        distSignal;
-        numOverlays;
-        angleOverlays;
-        distOverlays;
-        SNRs;
-        typeOverlays;
-        overlays;
-        offsetOverlays;
+    properties (SetAccess = protected)
+        sources; % sources(1) is the main source, data will be from pipeline data
+        SNRs; % SNRs(1) is always interpreted as 0
+              % all others are in relation to sources(1)
+              % length(sources) must be == length(SNRs)
         room;
     end
 
@@ -18,108 +13,80 @@ classdef SceneConfiguration < handle
     methods
         
         function obj = SceneConfiguration() % creates a "clean" configuration
-            obj.angleSignal = sceneConfig.ValGen( 'manual', 0 );
-            obj.distSignal = sceneConfig.ValGen( 'manual', 3 );
-            obj.numOverlays = 0;
-            obj.room = sceneConfig.ValGen( 'manual', [] );
-            obj.angleOverlays = sceneConfig.ValGen.empty;
-            obj.distOverlays = sceneConfig.ValGen.empty;
+            obj.room = sceneConfig.RoomValGen.empty;
             obj.SNRs = sceneConfig.ValGen.empty;
-            obj.typeOverlays = cell(0);
-            obj.overlays = sceneConfig.ValGen.empty;
-            obj.offsetOverlays= sceneConfig.ValGen.empty;
+            obj.sources = sceneConfig.SourceBase.empty;
         end
         %% -------------------------------------------------------------------------------
-
-        function addOverlay( obj, angle, dist, SNR, type, file, offset_s )
-            obj.numOverlays = obj.numOverlays + 1;
-            if ~isa( angle, 'sceneConfig.ValGen' ), error( 'Use a sceneConfig.ValGen' ); end;
-            obj.angleOverlays(obj.numOverlays) = angle;
-            if ~isa( dist, 'sceneConfig.ValGen' ), error( 'Use a sceneConfig.ValGen' ); end;
-            obj.distOverlays(obj.numOverlays) = dist;
-            if ~isa( SNR, 'sceneConfig.ValGen' ), error( 'Use a sceneConfig.ValGen' ); end;
-            obj.SNRs(obj.numOverlays) = SNR;
-            if sum( strcmpi( type, {'point', 'diffuse'} ) ) == 0
-                error( 'Unknown overlay type' );
+        
+        function addSource( obj, source, snr )
+            obj.sources(end+1) = source;
+            if numel( obj.SNRs ) == 0  || nargin < 3
+                obj.SNRs(end+1) = sceneConfig.ValGen( 'manual', 0 );
+            else
+                obj.SNRs(end+1) = snr;
             end
-            obj.typeOverlays{obj.numOverlays} = type;
-            if ~isa( file, 'sceneConfig.ValGen' ), error( 'Use a sceneConfig.ValGen' ); end;
-            obj.overlays(obj.numOverlays) = file;
-            if ~isa( offset_s, 'sceneConfig.ValGen' ), error( 'Use a sceneConfig.ValGen' ); end;
-            obj.offsetOverlays(obj.numOverlays) = offset_s;
         end
         %% -------------------------------------------------------------------------------
         
         function addRoom( obj, room )
-            if ~isa( room, 'sceneConfig.RoomValGen' ), error( 'Use a sceneConfig.RoomValGen' ); end;
             obj.room = room;
         end
         %% -------------------------------------------------------------------------------
 
         function confInst = instantiate( obj )
             confInst = sceneConfig.SceneConfiguration();
-            confInst.angleSignal = sceneConfig.ValGen( 'manual', obj.angleSignal.value );
-            confInst.distSignal = sceneConfig.ValGen( 'manual', obj.distSignal.value );
-            confInst.numOverlays = obj.numOverlays;
-            confInst.room = sceneConfig.ValGen( 'manual', obj.room.value );
-            for kk = 1:obj.numOverlays
-                confInst.angleOverlays(kk) = sceneConfig.ValGen( 'manual', obj.angleOverlays(kk).value );
-                confInst.distOverlays(kk) = sceneConfig.ValGen( 'manual', obj.distOverlays(kk).value );
-                confInst.SNRs(kk) = sceneConfig.ValGen( 'manual', obj.SNRs(kk).value );
-                confInst.typeOverlays{kk} = obj.typeOverlays{kk};
-                confInst.overlays(kk) = sceneConfig.ValGen( 'manual', obj.overlays(kk).value );
-                confInst.offsetOverlays(kk) = sceneConfig.ValGen( 'manual', obj.offsetOverlays(kk).value );
+            confInst.room = obj.room.instantiate();
+            for kk = 1 : numel( obj.sources )
+                confInst.sources(kk) = obj.sources(kk).instantiate();
+                confInst.SNRs(kk) = obj.SNRs(kk).instantiate();
             end
         end
         %% -------------------------------------------------------------------------------
 
-        function splittedConfs = split( obj )
-            splittedConfs(1) = sceneConfig.SceneConfiguration();
-            splittedConfs(1).angleSignal = copy( obj.angleSignal );
-            splittedConfs(1).distSignal = copy( obj.distSignal );
-            splittedConfs(1).room = copy( obj.room );
-            for kk = 1:obj.numOverlays
-                splittedConfs(kk+1) = sceneConfig.SceneConfiguration();
-                splittedConfs(kk+1).numOverlays = 1;
-                splittedConfs(kk+1).angleOverlays(1) = copy( obj.angleOverlays(kk) );
-                splittedConfs(kk+1).distOverlays(1) = copy( obj.distOverlays(kk) );
-                splittedConfs(kk+1).SNRs(1) = copy( obj.SNRs(kk) );
-                splittedConfs(kk+1).typeOverlays{1} = obj.typeOverlays{kk};
-                splittedConfs(kk+1).overlays(1) = copy( obj.overlays(kk) );
-                splittedConfs(kk+1).offsetOverlays(1) = copy( obj.offsetOverlays(kk) );
-            end
+        function singleConfig = getSingleConfig( obj, srcIdx )
+            singleConfig = sceneConfig.SceneConfiguration();
+            singleConfig.room = obj.room;
+            singleConfig.sources = obj.sources(srcIdx);
+            singleConfig.SNRs = sceneConfig.ValGen( 'manual', 0 );
         end
         %% -------------------------------------------------------------------------------
         
-        function e = isequal( obj, cObj )
-            if ~isequaln( obj.numOverlays, cObj.numOverlays )
-                e = false;
-                return;
-            end
-            for kk = 1 : obj.numOverlays
-                if ~isequal( obj.overlays(kk), cObj.overlays(kk) )
-                    e = false;
-                    return;
+        function e = isequal( obj1, obj2 )
+            e = false;
+            if isempty( obj1 ) && isempty( obj2 ), e = true; return; end
+            if isempty( obj1 ) || isempty( obj2 ), return; end
+            if numel( obj1.sources ) ~= numel( obj2.sources ), return; end
+            obj2srcsInCmpIdxs = ones( size( obj2.sources ) );
+            for kk = 1 : numel( obj1.sources )
+                sequal = sceneConfig.SourceBase.isequalHetrgn( obj1.sources(kk), obj2.sources ) & obj2srcsInCmpIdxs;
+                if ~any( sequal ), return; 
+                else
+                    ssequal = isequal( obj1.SNRs(kk), obj2.SNRs ) & sequal;
+                    if ~any( ssequal ), return; 
+                    else
+                        sseFirstIdx = find( ssequal == 1, 1, 'first' );
+                        obj2srcsInCmpIdxs(sseFirstIdx) = 0;
+                    end
                 end
             end
-            if isempty( obj.room.value ) && isprop( cObj, 'walls' ) ...
-                    && isempty( cObj.walls.value ) % compatibility to former walls prop
-                wallsRoomEq = true;
-            else
-                wallsRoomEq = isequaln( obj.room, cObj.room );
-            end
-            e = ...
-                isequaln( obj.angleSignal, cObj.angleSignal) && ...
-                isequaln( obj.distSignal, cObj.distSignal ) && ...
-                isequaln( obj.angleOverlays, cObj.angleOverlays ) && ...
-                isequaln( obj.distOverlays, cObj.distOverlays ) && ...
-                isequaln( obj.SNRs, cObj.SNRs ) && ...
-                isequaln( obj.typeOverlays, cObj.typeOverlays ) && ...
-                isequaln( obj.offsetOverlays, cObj.offsetOverlays ) && ...
-                wallsRoomEq;
+            e = isequal( obj1.room, obj2.room );
         end
         %% -------------------------------------------------------------------------------
         
     end
     
+    methods (Access = protected)
+        
+        function csc = copyElement( obj )
+            csc = sceneConfig.SceneConfiguration();
+            for ii = 1 : numel( obj.sources )
+                csc.sources(ii) = copy( obj.sources(ii) );
+                csc.SNRs(ii) = copy( obj.SNRs(ii) );
+            end
+            csc.room = copy( obj.room );
+        end
+        %% -------------------------------------------------------------------------------
+    end
+
 end

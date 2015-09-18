8febdcddc2c38a7fab41b2b6dea0d1ed22737026
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-09-18 17:41:02 +0200

    small fixes in test scripts

--- test/trainAndTestCleanModel_2.m
+++ test/trainAndTestCleanModel_2.m
@@ -10,12 +10,12 @@ pipe = TwoEarsIdTrainPipe();
 pipe.featureCreator = featureCreators.FeatureSet1Blockmean();
 pipe.modelCreator = modelTrainers.GlmNetLambdaSelectTrainer( ...
     'performanceMeasure', @performanceMeasures.BAC2, ...
-    'cvFolds', 7, ...
+    'cvFolds', 4, ...
     'alpha', 0.99 );
 pipe.modelCreator.verbose( 'on' );
 
-pipe.trainset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_80pTrain_TrainSet_1.flist';
-pipe.testset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_80pTrain_TestSet_1.flist';
+pipe.trainset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_mini_TrainSet.flist';
+pipe.testset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_mini_TestSet.flist';
 
 sc = sceneConfig.SceneConfiguration();
 sc.addSource( sceneConfig.PointSource() );

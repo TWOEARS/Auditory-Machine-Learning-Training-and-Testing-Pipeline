22fa04068881fdf074df59a7539a0447332511ad
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-10-15 12:09:20 +0200

    data script

--- test/expsDataGen/saveGenOvrl.m
+++ test/expsDataGen/saveGenOvrl.m
@@ -83,24 +83,24 @@ pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 % pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 
 
-% pipe = TwoEarsIdTrainPipe();
-% pipe.featureCreator = fc;
-% pipe.modelCreator = modelTrainers.LoadModelNoopTrainer( 'noop' );
-% pipe.modelCreator.verbose( 'on' );
-% 
-% pipe.data = fl;
-% pipe.trainsetShare = 1;
-% pipe.setupData();
-% 
-% sc = sceneConfig.SceneConfiguration();
-% sc.addSource( sceneConfig.PointSource() );
-% sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
-%     'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
-%     sceneConfig.ValGen( 'manual', 20 ));
-% pipe.setSceneConfig( sc ); 
-% 
-% pipe.init();
-% pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = fc;
+pipe.modelCreator = modelTrainers.LoadModelNoopTrainer( 'noop' );
+pipe.modelCreator.verbose( 'on' );
+
+pipe.data = fl;
+pipe.trainsetShare = 1;
+pipe.setupData();
+
+sc = sceneConfig.SceneConfiguration();
+sc.addSource( sceneConfig.PointSource() );
+sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', 20 ));
+pipe.setSceneConfig( sc ); 
+
+pipe.init();
+pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 
 
 
@@ -204,24 +204,24 @@ pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 % pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 
 
-pipe = TwoEarsIdTrainPipe();
-pipe.featureCreator = fc;
-pipe.modelCreator = modelTrainers.LoadModelNoopTrainer( 'noop' );
-pipe.modelCreator.verbose( 'on' );
-
-pipe.data = fl;
-pipe.trainsetShare = 1;
-pipe.setupData();
-
-sc = sceneConfig.SceneConfiguration();
-sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',-90) ) );
-sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
-    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
-    sceneConfig.ValGen( 'manual', 20 ));
-pipe.setSceneConfig( sc ); 
-
-pipe.init();
-pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
+% pipe = TwoEarsIdTrainPipe();
+% pipe.featureCreator = fc;
+% pipe.modelCreator = modelTrainers.LoadModelNoopTrainer( 'noop' );
+% pipe.modelCreator.verbose( 'on' );
+% 
+% pipe.data = fl;
+% pipe.trainsetShare = 1;
+% pipe.setupData();
+% 
+% sc = sceneConfig.SceneConfiguration();
+% sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',-90) ) );
+% sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
+%     'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+%     sceneConfig.ValGen( 'manual', 20 ));
+% pipe.setSceneConfig( sc ); 
+% 
+% pipe.init();
+% pths{end+1} = pipe.pipeline.run( {'dataStoreUni'}, 0 );
 
 
 end

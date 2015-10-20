ff52e94041eb0e1c474d099ef6bd3b80d60c6bed
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-10-20 16:42:07 +0200

    save scripts

--- test/expsDataGen/saveGO_set1.m
+++ test/expsDataGen/saveGO_set1.m
@@ -9,20 +9,32 @@ pths = {};
 featureCreator = featureCreators.FeatureSet1Blockmean();
 dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_1.flist';
 
-pths{end+1} = saveGenOvrl( featureCreator, dataset );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 20 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 20 );
 
 dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_1.flist';
 
-pths{end+1} = saveGenOvrl( featureCreator, dataset );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 20 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 20 );
 
-% featureCreator = featureCreators.FeatureSet1Blockmean2Ch();
-% dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_1.flist';
-% 
-% pths{end+1} = saveGenOvrl2( featureCreator, dataset );
-% 
-% dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_1.flist';
-% 
-% pths{end+1} = saveGenOvrl2( featureCreator, dataset );
+featureCreator = featureCreators.FeatureSet1Blockmean2Ch();
+dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_1.flist';
+
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 20 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 20 );
+
+dataset = 'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_1.flist';
+
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 0 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, 0, 0, 20 );
+pths{end+1} = saveGenOvrl( featureCreator, dataset, -45, 45, 20 );
 
 
 end

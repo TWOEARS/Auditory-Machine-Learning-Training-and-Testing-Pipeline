02b21d1da4f4fd927500aff28e802a0a85d760f1
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-10-27 02:16:46 +0100

    added expsFeatSelect gos & mc1

---
+++ test/expsFeatSelect/trainGlmNet_GOs.m
@@ -0,0 +1,44 @@
+function trainGlmNet_GOs( classname )
+    
+addpath( '../..' );
+startIdentificationTraining();
+
+featureCreators = {?featureCreators.FeatureSet1Blockmean2Ch,...
+                   ?featureCreators.FeatureSet1Blockmean,...
+                   ?featureCreators.FeatureSet1VarBlocks,...
+                   ?featureCreators.FeatureSet1BlockmeanLowVsHighFreqRes};
+azimuths = {{0,0},{-45,45},{-90,90}};
+snrs = {20,10,0,-10};
+
+for fc = 1 : numel( featureCreators )
+for ss = 1 : numel( snrs )
+for aa = 1 : numel( azimuths )
+    
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = feval( featureCreators{fc}.Name );
+pipe.modelCreator = modelTrainers.GlmNetLambdaSelectTrainer( ...
+    'performanceMeasure', @performanceMeasures.BAC2, ...
+    'cvFolds', 7, ...
+    'alpha', 0.99 );
+pipe.modelCreator.verbose( 'on' );
+
+setsBasePath = 'learned_models/IdentityKS/trainTestSets/';
+pipe.trainset = [setsBasePath 'NIGENS_75pTrain_TrainSet_1.flist'];
+pipe.setupData();
+
+sc = sceneConfig.SceneConfiguration();
+sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',azimuths{aa}{1}) ) );
+sc.addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',azimuths{aa}{2}), ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', snrs{ss} ));
+pipe.setSceneConfig( sc ); 
+
+pipe.init();
+modelpathes{ss,fc,aa} = pipe.pipeline.run( {classname}, 0 );
+
+save( ['glmnet_gos_' classname '.mat'], 'snrs', 'featureCreators', 'azimuths', 'modelpathes' );
+
+end
+end
+end
+
---
+++ test/expsFeatSelect/trainGlmNet_mc1.m
@@ -0,0 +1,85 @@
+function trainGlmNet_mc1( classname )
+
+%startTwoEars( '../IdentificationTraining.xml' );
+addpath( '../..' );
+startIdentificationTraining();
+
+featureCreators = {?featureCreators.FeatureSet1Blockmean,...
+                   ?featureCreators.FeatureSet1VarBlocks,...
+                   ?featureCreators.FeatureSet1BlockmeanLowVsHighFreqRes};
+
+for fc = 1 : numel( featureCreators )
+    
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = feval( featureCreators{fc}.Name );
+pipe.modelCreator = modelTrainers.GlmNetLambdaSelectTrainer( ...
+    'performanceMeasure', @performanceMeasures.BAC2, ...
+    'cvFolds', 7, ...
+    'alpha', 0.99, ...
+    'maxDataSize', 200000 );
+modelTrainers.Base.balMaxData( true, true );
+pipe.modelCreator.verbose( 'on' );
+
+setsBasePath = 'learned_models/IdentityKS/trainTestSets/';
+pipe.trainset = [setsBasePath 'NIGENS_75pTrain_TrainSet_1.flist'];
+pipe.setupData();
+
+sc(1) = sceneConfig.SceneConfiguration();
+sc(1).addSource( sceneConfig.PointSource() );
+sc(1).addSource( sceneConfig.PointSource( ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', 0 ));
+
+sc(2) = sceneConfig.SceneConfiguration();
+sc(2).addSource( sceneConfig.PointSource() );
+sc(2).addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', 0 ));
+
+sc(3) = sceneConfig.SceneConfiguration();
+sc(3).addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',-45) ) );
+sc(3).addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',45), ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', 0 ));
+
+sc(4) = sceneConfig.SceneConfiguration();
+sc(4).addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',-90) ) );
+sc(4).addSource( sceneConfig.PointSource( 'azimuth',sceneConfig.ValGen('manual',90), ...
+    'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')) ),...
+    sceneConfig.ValGen( 'manual', 0 ));
+
+sc(5) = sceneConfig.SceneConfiguration();
+sc(5).addSource( sceneConfig.PointSource('azimuth',sceneConfig.ValGen('manual',0)) );
+
+sc(6) = sceneConfig.SceneConfiguration();
+sc(6).addSource( sceneConfig.PointSource('azimuth',sceneConfig.ValGen('manual',135)) );
+
+sc(7) = sceneConfig.SceneConfiguration();
+sc(7).addSource( sceneConfig.PointSource() );
+sc(7).addSource( sceneConfig.DiffuseSource( ...
+    'data',sceneConfig.NoiseValGen(struct( 'len', sceneConfig.ValGen('manual',4410000) )) ),...
+    sceneConfig.ValGen( 'manual', -10 ));
+
+sc(8) = sceneConfig.SceneConfiguration();
+sc(8).addSource( sceneConfig.PointSource() );
+sc(8).addSource( sceneConfig.DiffuseSource( ...
+    'data',sceneConfig.NoiseValGen(struct( 'len', sceneConfig.ValGen('manual',4410000) )) ),...
+    sceneConfig.ValGen( 'manual', 0 ));
+
+sc(9) = sceneConfig.SceneConfiguration();
+sc(9).addSource( sceneConfig.PointSource() );
+sc(9).addSource( sceneConfig.DiffuseSource( ...
+    'data',sceneConfig.NoiseValGen(struct( 'len', sceneConfig.ValGen('manual',4410000) )) ),...
+    sceneConfig.ValGen( 'manual', 10 ));
+
+pipe.setSceneConfig( sc ); 
+
+pipe.init();
+modelpathes{fc} = pipe.pipeline.run( {classname}, 0 );
+
+save( ['glmnet_mc1_' classname '.mat'], 'featureCreators', 'modelpathes' );
+
+end
+
+
+

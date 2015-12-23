a1ce9f71b16deae21465d85010dd082a7151f90f
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2015-12-23 16:12:34 +0100

    added expsDataGen/gen_PDs.m

---
+++ test/expsDataGen/gen_PDs.m
@@ -0,0 +1,56 @@
+function gen_PDs( azmCfgIdxs )
+    
+addpath( '../..' );
+startIdentificationTraining();
+
+featureCreators = {?featureCreators.FeatureSet1Blockmean2Ch,...
+                   ?featureCreators.FeatureSet1Blockmean};
+azimuths = {{0,0},...
+            {0,45},{45,0},{22.5,-22.5},{67.5,112.5},{-157.5,157.5},...
+            {0,90},{22.5,112.5},{45,135},{90,180},{22.5,-67.5},{45,-45},{90,0},{-157.5,112.5},...
+            {0,180},{22.5,-157.5},{45,-135},{67.5,-112.5},{90,-90}}; % 19 cfgs
+snrs = {10,0,-10,-20};
+datasets = {'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_1.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_1.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_2.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_2.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_3.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_3.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TrainSet_4.flist',...
+            'learned_models/IdentityKS/trainTestSets/NIGENS_75pTrain_TestSet_4.flist'
+            };
+
+for ff = 1 : numel( featureCreators )
+for ss = 1 : numel( snrs )
+for aa = azmCfgIdxs
+for dd = 1 : numel( datasets )
+
+fprintf( '\n\n==============\nStarting; dd = %d, ff = %d, ss = %d, aa = %d.==============\n\n', dd, ff, ss, aa );
+
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = feval( featureCreators{ff}.Name );
+pipe.modelCreator = modelTrainers.LoadModelNoopTrainer( 'noop' );
+pipe.modelCreator.verbose( 'on' );
+
+pipe.data = datasets{dd};
+pipe.trainsetShare = 1;
+pipe.setupData();
+
+sc = sceneConfig.SceneConfiguration();
+sc.addSource( sceneConfig.PointSource( ...
+        'azimuth',sceneConfig.ValGen('manual',azimuths{aa}{1}) ) );
+sc.addSource( sceneConfig.PointSource( ...
+        'azimuth',sceneConfig.ValGen('manual',azimuths{aa}{2}), ...
+        'data',sceneConfig.FileListValGen(pipe.pipeline.data('general',:,'wavFileName')),...
+        'offset', sceneConfig.ValGen('manual',0.0) ),...
+    sceneConfig.ValGen( 'manual', snrs{ss} ),...
+    true ); % loop
+pipe.setSceneConfig( [sc] ); 
+
+pipe.init();
+pipe.pipeline.run( {'donttrain'}, 0 );
+
+end
+end
+end
+end

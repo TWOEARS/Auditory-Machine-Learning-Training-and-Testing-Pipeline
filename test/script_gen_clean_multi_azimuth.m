7ebc55399693f9b11117628045c8dd377d5d8131
Youssef Kashef <youssef.kashef@gmail.com>
2016-05-26 10:36:16 +0200

    save train and test paths

--- test/script_gen_clean_multi_azimuth.m
+++ test/script_gen_clean_multi_azimuth.m
@@ -2,7 +2,7 @@ startIdentificationTraining;
 target_azimuths = [-90, -45, 0, 45, 90];
 for ii=1:length(target_azimuths)
     feat_creator = featureCreators.FeatureSetRawRmAmsLRSeparate();
-    modelPath_lr_separate{ii} = gen_clean_at_azimuth( target_azimuths(ii), feat_creator );
+    [modelPath_train{ii}, modelPath_test{ii}] = gen_clean_at_azimuth( target_azimuths(ii), feat_creator );
 end
 
-save('modelPath_lr_separate.mat', 'modelPath_lr_separate');
+save('modelPath.mat', 'modelPath_train', 'modelPath_test');

804a1795193c180ddd483cbd930a532e89751e1e
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2016-07-21 18:21:47 +0200

    replaced BuildAndStoreCleanData.m by buildAndStoreCleanMulticlassData.m

---
+++ test/buildAndStoreCleanMulticlassData.m
@@ -0,0 +1,24 @@
+function buildAndStoreCleanMulticlassData( )
+
+addPathsIfNotIncluded( cleanPathFromRelativeRefs( [pwd '/..'] ) ); 
+startIdentificationTraining();
+
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = FeatureCreators.FeatureSetRmBlockmean();
+% alarm will be 1, baby 2, female 3, fire 4, rest -1
+typeMulticlassLabeler = LabelCreators.MultiEventTypeLabeler( ...
+                                'types', {{'alarm'},{'baby'},{'femaleSpeech'},{'fire'}} );
+pipe.labelCreator = typeMulticlassLabeler;
+pipe.modelCreator = ModelTrainers.LoadModelNoopTrainer( 'noop' );
+
+pipe.data = 'learned_models\IdentityKS\trainTestSets/NIGENS_mini_TrainSet_1.flist';
+pipe.setupData();
+
+sc = SceneConfig.SceneConfiguration();
+sc.addSource( SceneConfig.PointSource( ...
+        'data', SceneConfig.FileListValGen( 'pipeInput' )  )  );
+pipe.init( sc );
+
+modelPath = pipe.pipeline.run( 'runOption', 'dataStoreUni', 'modelPath', 'cleanMulticlassData' );
+
+fprintf( ' -- run log is saved at %s -- \n\n', modelPath );

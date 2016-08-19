f427a03db0cf2da7fed43778d84ef5a1226d715e
Youssef Kashef <youssef.kashef@gmail.com>
2016-08-19 11:47:48 +0200

    add label creator to generate combined identification + azimuth distribution matrix output

--- src/+LabelCreators/IdAzmDistributionLabeler.m
+++ src/+LabelCreators/IdAzmDistributionLabeler.m
@@ -1,9 +1,8 @@
-classdef AzmDistributionLabeler < LabelCreators.EnergyDependentLabeler
+classdef IdAzmDistributionLabeler < LabelCreators.AzmDistributionLabeler
     % class for labeling blocks by azm of a specified source
     %% -----------------------------------------------------------------------------------
     properties (SetAccess = private)
-        angularResolution;
-        nAngles;
+        types;
     end
     
     %% -----------------------------------------------------------------------------------
@@ -13,30 +12,38 @@ classdef AzmDistributionLabeler < LabelCreators.EnergyDependentLabeler
     %% -----------------------------------------------------------------------------------
     methods
         
-        function obj = AzmDistributionLabeler( varargin )
+        function obj = IdAzmDistributionLabeler( varargin )
             ip = inputParser;
-            ip.addOptional( 'angularResolution', 15 );
+            ip.addOptional( 'angularResolution', 5 );
             ip.addOptional( 'sourcesMinEnergy', -20 );
             ip.addOptional( 'labelBlockSize_s', [] );
-            ip.addOptional( 'sourceIds', ':' );
+            ip.addOptional( 'types', {{'Type1'},{'Type2'}} );
             ip.parse( varargin{:} );
-            obj = obj@LabelCreators.EnergyDependentLabeler( ...
+            obj = obj@LabelCreators.AzmDistributionLabeler( ...
+                                      'angularResolution', ip.Results.angularResolution, ...
                                       'labelBlockSize_s', ip.Results.labelBlockSize_s, ...
-                                      'sourcesMinEnergy', ip.Results.sourcesMinEnergy, ...
-                                      'sourceIds', ip.Results.sourceIds );
-            obj.angularResolution = ip.Results.angularResolution;
-            obj.nAngles = 360 / obj.angularResolution;
-            if rem( obj.nAngles, 1 ) ~= 0
-                error( 'Choose a divisor of 360 as angularResolution.' );
-            end
+                                      'sourcesMinEnergy', ip.Results.sourcesMinEnergy);
+            obj.types = ip.Results.types;
         end
         %% -------------------------------------------------------------------------------
         
         function y = labelEnergeticBlock( obj, blockAnnotations )
             srcAzms = blockAnnotations.srcAzms(obj.sourceIds,:);
             srcAzmIdxs = mod( round( srcAzms / obj.angularResolution ) + 1, obj.nAngles );
-            y = zeros( 1, obj.nAngles );
-            y(srcAzmIdxs) = 1;
+            y = zeros( numel(obj.types), obj.nAngles+1 );
+            y(:, end) = 1; % set void bin to 1
+            typeAtSrc = blockAnnotations.srcType.srcType(:,1);
+            srcIdx = blockAnnotations.srcType.srcType(:,2);
+            for ii = 1:numel(srcIdx)
+                isType = cellfun(@(v) strcmp([v{:}], typeAtSrc(ii)), ...
+                                obj.types, 'un', false);
+                typeIdx = find(not(cellfun('isempty', isType)));
+                if ~isempty(typeIdx)
+                    y(typeIdx, srcAzmIdxs(srcIdx{ii})) = 1;
+                    y(typeIdx, end) = 0; % clear void bin
+                end
+            end
+            y = reshape(y, 1, numel(obj.types) * (obj.nAngles + 1));
         end
         
     end
@@ -46,6 +53,7 @@ classdef AzmDistributionLabeler < LabelCreators.EnergyDependentLabeler
         %% -----------------------------------------------------------------------------------
         function outputDeps = getLabelInternOutputDependencies( obj )
             outputDeps.angularResolution = obj.angularResolution;
+            outputDeps.types = obj.types;
             outputDeps.v = 1;
         end   
     end
---
+++ test/build4PointSrcsAzmDistrData.m
@@ -0,0 +1,31 @@
+function buildMultiPointSrcsAzmDistrData()
+
+addPathsIfNotIncluded( cleanPathFromRelativeRefs( [pwd '/..'] ) ); 
+startIdentificationTraining();
+
+pipe = TwoEarsIdTrainPipe();
+pipe.featureCreator = FeatureCreators.FeatureSetRmAmsBlockmean();
+azmLabeler = LabelCreators.IdAzmDistributionLabeler( 'angularResolution', 5, ...
+    'types', {{'alarm'},{'baby'},{'femaleSpeech'},{'fire'}});
+pipe.labelCreator = azmLabeler;
+pipe.modelCreator = ModelTrainers.LoadModelNoopTrainer( 'noop' );
+pipe.modelCreator.verbose( 'on' );
+
+pipe.trainset = 'learned_models/IdentityKS/trainTestSets/NIGENS160807_mini_TrainSet_1.flist';
+pipe.setupData();
+
+sc(1) = SceneConfig.SceneConfiguration();
+sc(1).addSource( SceneConfig.PointSource( ...
+        'data', SceneConfig.FileListValGen( 'pipeInput' ),...
+        'azimuth', SceneConfig.ValGen( 'manual', 90 ) ) );
+sc(1).addSource( SceneConfig.PointSource( ...
+        'azimuth',SceneConfig.ValGen( 'manual', -90 ),...
+        'data', SceneConfig.FileListValGen( pipe.pipeline.trainSet(:,'fileName') ),...
+        'offset', SceneConfig.ValGen( 'manual', 0 ) ),...
+        'loop', 'randomSeq' );
+pipe.init( sc );
+
+modelPath = pipe.pipeline.run( 'modelPath', 'test_multivarData', 'runOption', 'dataStoreUni' );
+
+fprintf( ' -- Model is saved at %s -- \n\n', modelPath );
+

e6a4ef9a0291e4f7f2dbefdcf6570aa5fcc2ef64
Youssef Kashef <youssef.kashef@gmail.com>
2016-09-15 17:00:41 +0200

    handle network output and initialize network after construction

--- src/+Models/CaffeModel.m
+++ src/+Models/CaffeModel.m
@@ -4,6 +4,13 @@ classdef CaffeModel < Models.Base
     % the LD_LIBRARY_PATH environment variable prior to launching MATLAB)
     %% --------------------------------------------------------------------
     properties (SetAccess = ?ModelTrainers.CaffeModel)
+        fpath_net_def;
+        fpath_weights;
+        thr;            % output node thresholds, default is 0.5
+        has_thr;
+    end
+    
+    properties (SetAccess = ?ModelTrainers.CaffeModel, Transient=false)
         net;
     end
     
@@ -11,32 +18,59 @@ classdef CaffeModel < Models.Base
     methods
 
         function obj = CaffeModel(dir_matcaffe, ...
-                fpath_net_def, fpath_weights, use_gpu)
+                modelDir, fname_net_def, fname_weights, ...
+                thr)
            % Add caffe/matlab to our Matlab search PATH to use matcaffe
             if exist([dir_matcaffe, filesep, '+caffe'], 'dir')
                 addpath(dir_matcaffe);
             else
                 error('Could not find matcaffe.');
             end
-            if ~exist(fpath_net_def, 'file')
-                error('Could not find network definition prototxt.');
-            end
-            if ~exist(fpath_weights, 'file')
-                error('Could not find network weights file.');
-            end
-            % Set caffe mode
-            if exist('use_gpu', 'var') && use_gpu
-                caffe.set_mode_gpu();
-                gpu_id = 0;  % we will use the first gpu in this demo
-                caffe.set_device(gpu_id);
-                verboseFprintf( obj, 'Using caffe in GPU mode, device id:%d.\n', gpu_id );
+            obj.net = [];
+            initNet(modelDir, fname_net_def, fname_weights);
+            if exist('thr', 'var') && ~isempty(thr)
+                obj.thr = thr;
+                if ~isa(obj.thr, 'cell')
+                    obj.thr = {obj.thr};
+                end
+                obj.has_thr = true;
             else
-                caffe.set_mode_cpu();
-                verboseFprintf( obj, 'Using caffe in CPU mode.\n' );
+                obj.has_thr = false;
             end
-            % Initialize a network
+        end
+        %% -----------------------------------------------------------------
+        
+
+        %% -----------------------------------------------------------------
+        function initNet(obj, modelDir, fname_net_def, fname_weights)
+            % INITNET
+            % innitialize underlying caffe network object from definition
+            % and weight files
+            %% --------------------------------------------------------------------
+            
+%             if ~exist(modelDir, 'dir')
+%                 error('Could not find model directory %s.', modelDir);
+%             end
+%             if ~exist(fullfile(modelDir, fname_net_def), 'file')
+%                 error('Could not find network definition prototxt.');
+%             else
+%                 obj.fpath_net_def = fullfile(modelDir, fname_net_def);
+%             end
+%             if ~exist(fullfile(modelDir, fname_weights), 'file')
+%                 error('Could not find network weights file.');
+%             else
+%                 obj.fpath_weights = fullfile(modelDir, fname_weights);
+%             end
+
+            obj.fpath_net_def = xml.dbGetFile(fullfile(modelDir, fname_net_def));
+            obj.fpath_weights = xml.dbGetFile(fullfile(modelDir, fname_weights));
+            
             phase = 'test'; % run with phase test (so that dropout isn't applied)
-            obj.net = caffe.Net(fpath_net_def, fpath_weights, phase);  
+            if ~isempty(obj.net)
+                delete(obj.net);
+                clear obj.net;
+            end
+            obj.net = caffe.Net(obj.fpath_net_def, obj.fpath_weights, phase);
         end
         %% -----------------------------------------------------------------
         
@@ -48,8 +82,8 @@ classdef CaffeModel < Models.Base
         function [y,score] = applyModelMasked( obj, x )
             blobs_in = x{1};
             blobs_in_names = x{2};
-            %blobs_in_idx = cellfun(@(v) find(strcmp(v, {'ratemap','amsFeatures'})),{'amsFeatures', 'ratemap'}, 'un', false)
             data_in = cell(numel(obj.net.inputs));
+            % prepare input data by selecting required features
             for ii = 1:numel(obj.net.inputs)
                 for jj = 1:numel(blobs_in_names)
                     if strcmp(blobs_in_names{jj}, obj.net.inputs{ii})
@@ -57,22 +91,53 @@ classdef CaffeModel < Models.Base
                     end
                 end
             end
-            scores = obj.net.forward(data_in);
-            score = squeeze(scores);
-            y = 0;
+            % TODO: remove this line, only used for debugging at the
+            % momment
+            data_in_pad = {cat(1, zeros(13,16,2), data_in{1})};
+            blobs_out = obj.net.forward(data_in_pad);
+            %blobs_out = obj.net.forward(data_in);
+            % extract predictions from network
+            score = {};
+            y = {};
+            for ii = 1:numel(obj.net.outputs)
+                score.(obj.net.outputs{ii}) = double(blobs_out{ii});
+                if obj.has_thr
+                    y.(obj.net.outputs{ii}) = blobs_out{ii} >= obj.thr{ii};
+                else
+                    y.(obj.net.outputs{ii}) = blobs_out{ii} >= 0.5;
+                end
+            end
         end
         %% -----------------------------------------------------------------
 
+        function delete(obj)
+            % DELETE Destructor
+            
+            % Shut down the network
+            delete(obj.net);
+            clear obj.net;
+        end
+        
     end
     
     methods (Static)
 
-        %% -------------------------------------------------------------------------------
-        function x_new = prepare_input( x )
-            % format: W x H x C with BGR channels
-            x_new = x(:, :, [3, 2, 1]);  % permute channels from RGB to BGR
-            x_new = permute(x_new, [2, 1, 3]);  % flip width and height
+        function setMode(use_gpu, gpu_id)
+            % Set caffe mode (gpu or cpu mode)
+            if exist('use_gpu', 'var') && use_gpu
+                caffe.set_mode_gpu();
+                if ~exist('gpu_id', 'var')
+                    gpu_id = 0;  % we will use the first gpu in this demo
+                end
+                caffe.set_device(gpu_id);
+                verboseFprintf( obj, 'Using caffe in GPU mode, device id:%d.\n', gpu_id );
+            else
+                caffe.set_mode_cpu();
+                verboseFprintf( obj, 'Using caffe in CPU mode.\n' );
+            end
         end
+        %% -----------------------------------------------------------------
+
     end
     
 end

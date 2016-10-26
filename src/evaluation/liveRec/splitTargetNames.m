414cb28846fe47bf41a1af7ee9e85bff4d8ee39d
Youssef Kashef <youssef.kashef@gmail.com>
2016-10-26 19:06:02 +0200

    reconcile differences in representing grouped classes

--- src/evaluation/liveRec/splitTargetNames.m
+++ src/evaluation/liveRec/splitTargetNames.m
@@ -1,7 +1,5 @@
 function [target_names] = splitTargetNames(fpath)
 
 [~, fname, ~] = fileparts( fpath );
-c = strsplit( fname, '-' );
-target_names = strsplit( c{1}, '_' );
-
+target_names = strsplit( fname, '_' );
 end

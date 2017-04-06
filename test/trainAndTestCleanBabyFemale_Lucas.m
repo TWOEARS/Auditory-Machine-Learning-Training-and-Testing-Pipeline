4eb4a18f89a40b82809081b4811837d9e17b0565
Lucas <lbuerschaper@hotmail.com>
2017-04-06 10:14:30 +0200

    test script

--- test/trainAndTestCleanBabyFemale_Lucas.m
+++ test/trainAndTestCleanBabyFemale_Lucas.m
@@ -8,14 +8,13 @@ pipe.featureCreator = FeatureCreators.FeatureSet1Blockmean(); % feature creator 
 babyFemaleVsRestLabeler = ... 
     LabelCreators.MultiEventTypeLabeler( 'types', {{'speech'}}, ...
                                           'negOut', 'rest' ); % Ziel des Trainings (type (x), location, number of sources)
-% binäre classification (hit or not) -> hier zB femaleSpeech oder
-% maleSpeech
+% binäre classification (hit or not) -> hier zB speech
 pipe.labelCreator = babyFemaleVsRestLabeler;
 pipe.modelCreator = ModelTrainers.GlmNetLambdaSelectTrainer( ... %was ist Trainingsalgo
-    'performanceMeasure', @PerformanceMeasures.BAC2, ... %todo anschauen
+    'performanceMeasure', @PerformanceMeasures.BAC, ... %todo anschauen
     'cvFolds', 4, ...
     'alpha', 0.99 );
-pipe.modelCreator.verbose( 'on' );
+pipe.modelCreator.verbose( 'on' ); %Ausführung wird kommentiert (-verbose)
 
 pipe.trainset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_75pTrain_TrainSet_1.flist';
 pipe.testset = 'learned_models/IdentityKS/trainTestSets/IEEE_AASP_75pTrain_TestSet_1.flist';

7562456c5cb64f420b5d85df1213add9f6da0647
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2017-06-05 14:10:01 +0200

    SceneConfig.SceneConfiguration.m: added TODO about SNRref, which currently could introduce wrong SNR adjustment if set adversely.

--- src/+SceneConfig/SceneConfiguration.m
+++ src/+SceneConfig/SceneConfiguration.m
@@ -4,7 +4,7 @@ classdef SceneConfiguration < matlab.mixin.Copyable
     properties (SetAccess = protected)
         sources; 
         SNRs;
-        snrRefs;
+        snrRefs; % TODO: there should only be one scene-wide SNRref.
         loopSrcs; % 'no','self','randomSeq'
         room;
         brirHeadOrientIdx;

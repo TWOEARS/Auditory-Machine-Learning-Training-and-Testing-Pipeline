9b88f293a7e8d16e0714a491516629f8b1fcab3a
Lucas <lbuerschaper@hotmail.com>
2017-08-15 12:42:25 +0200

    bla

--- plotResults.m
+++ plotResults.m
@@ -6,8 +6,8 @@ file = load(sprintf('%s/result.mat', resDir{1}));
 
 base = cellstr(num2str([file.res.base]'));
 beta = cellstr(num2str([file.res.beta]'));
-
-labels = strcat({'b: '},base, {', beta: '}, beta)';
+    
+labels = unique(strcat({'b: '},base, {', beta: '}, beta)');
 x = 1:12;
 
 for i = 1:length(resDir)
@@ -17,10 +17,12 @@ for i = 1:length(resDir)
     
     figure;
     s = surf(x,y,z,'FaceAlpha',0.9);
-    s.EdgeAlpha = 0.9;
-    s.EdgeColor = [0.5 0.5 0.5];
-    colorbar;
-    set(gca,'xticklabel',labels, 'xtick', 1:12, 'xticklabelrotation', 45);
-    xlabel('sparse coding model')
-    ylabel('sparsity factor \gamma')
+    s.EdgeColor = [0.4 0.4 0.4];
+    s.FaceColor = 'interp';
+    set(gca,'xticklabel',labels, 'xtick', 1:12, 'xticklabelrotation', -45, 'clim',[0.4 1]);
+    xlabel('sparse coding model');
+    ylabel('sparsity factor \gamma');
+    zlabel('avg performance');
+    title(resDir{i})
+    view( -20, 30);
 end

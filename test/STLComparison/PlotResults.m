0e2566adc3f1313afdc647c1f37a26eb12033125
Lucas <lbuerschaper@hotmail.com>
2017-09-27 10:02:07 +0200

    fixes for mixed model training

--- test/STLComparison/PlotResults.m
+++ test/STLComparison/PlotResults.m
@@ -2,7 +2,7 @@ clear;
 
 labels = {'alarm', 'baby', 'femaleSpeech', 'fire'};
 
-path = 'diverse/b368_0.4_0.4_20000_20it 180000samples';
+path = 'diverse/b368_0.4_0.4_inf_inclNigens_100it 180000samples';
 %PlotResultsByClass(path, labels);
 PlotResultsOverall(path, labels, 10:10:100);
 
@@ -12,4 +12,4 @@ PlotResultsOverall(path, labels, 10:10:100);
 
 path = 'diverse/b368_0.4_0.4_inf_100it 180000samples';
 %PlotResultsByClass(path, labels);
-PlotResultsOverall(path, labels, 10:10:100);
+%PlotResultsOverall(path, labels, 10:10:100);

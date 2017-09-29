92ee847e7e0aaa5e416b03ee900f2735a72777f2
Lucas <lbuerschaper@hotmail.com>
2017-09-29 10:36:01 +0200

    some changes for plotting methods, fixed wrong model trainer in STLComparisonMixedSounds

--- test/STLComparison/PlotResults.m
+++ test/STLComparison/PlotResults.m
@@ -2,14 +2,14 @@ clear;
 
 labels = {'alarm', 'baby', 'femaleSpeech', 'fire'};
 
-path = 'diverse/b368_0.4_0.4_inf_inclNigens_100it 180000samples';
-%PlotResultsByClass(path, labels);
-PlotResultsOverall(path, labels, 10:10:100);
+path = 'diverse/b100_0.4_0.4_20000 20000samples';
+PlotResultsByClass(path, labels);
+%PlotResultsOverall(path, labels, 10:10:100);
 
-path = 'diverse/b368_0.4_0.4_inf_20it 180000samples';
-%PlotResultsByClass(path, labels);
-PlotResultsOverall(path, labels, 10:10:100);
+path = 'diverse/b100_0.4_0.4_100000_inclNigens 20000samples';
+PlotResultsByClass(path, labels);
+%PlotResultsOverall(path, labels, 10:10:100);
 
-path = 'diverse/b368_0.4_0.4_inf_100it 180000samples';
+%path = 'diverse/b368_0.4_0.4_inf_100it 180000samples';
 %PlotResultsByClass(path, labels);
 %PlotResultsOverall(path, labels, 10:10:100);

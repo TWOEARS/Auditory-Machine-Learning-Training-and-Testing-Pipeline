f06a30fc189f155405cb3c0b4db7f85f31a145ad
Ivo Trowitzsch <ivot@ni.tu-berlin.de>
2018-01-01 23:13:05 +0100

    alpha of MultiEventTypeTimeSeriesLabeler, added TimeSeriesLabelCreator

--- src/+LabelCreators/MultiEventTypeTimeSeriesLabeler.m
+++ src/+LabelCreators/MultiEventTypeTimeSeriesLabeler.m
@@ -1,17 +1,13 @@
-classdef MultiEventTypeTimeSeriesLabeler < LabelCreators.Base
+classdef MultiEventTypeTimeSeriesLabeler < LabelCreators.TimeSeriesLabelCreator
     % class for multi-class labeling blocks by event
     %% -----------------------------------------------------------------------------------
     properties (SetAccess = protected)
-        minBlockToEventRatio;
-        maxNegBlockToEventRatio;
         types;
         negOut;
         srcPrioMethod;
-        segIdTargetSrcFilter;
+        segIdTargetSrcFilter = [];
         srcTypeFilterOut;
-        nrgSrcsFilter;
-        fileFilterOut;
-        sourcesMinEnergy;
+        fileFilterOut = {};
     end
     
     %% -----------------------------------------------------------------------------------
@@ -27,30 +23,22 @@ classdef MultiEventTypeTimeSeriesLabeler < LabelCreators.Base
         
         function obj = MultiEventTypeTimeSeriesLabeler( varargin )
             ip = inputParser;
-            ip.addOptional( 'maxNegBlockToEventRatio', 0 );
-            ip.addOptional( 'labelBlockSize_s', [] );
-            ip.addOptional( 'removeUnclearBlocks', 'block-wise' );
+            ip.addOptional( 'removeUnclearBlocks', 'sequence-wise' );
             ip.addOptional( 'types', {{'Type1'},{'Type2'}} );
             ip.addOptional( 'negOut', 'rest' ); % rest, none
-            ip.addOptional( 'srcPrioMethod', 'order' ); % energy, order, time
-            ip.addOptional( 'segIdTargetSrcFilter', [] ); % e.g. [1,1;3,2]: throw away time-aggregate blocks with type 1 on other than src 1 and type 2 on other than src 3
+            ip.addOptional( 'srcPrioMethod', 'order' ); % energy, order
+%             ip.addOptional( 'segIdTargetSrcFilter', [] ); % e.g. [1,1;3,2]: throw away time-aggregate blocks with type 1 on other than src 1 and type 2 on other than src 3
             ip.addOptional( 'srcTypeFilterOut', [] ); % e.g. [2,1;3,2]: throw away type 1 blocks from src 2 and type 2 blocks from src 3
-            ip.addOptional( 'nrgSrcsFilter', [] ); % idxs of srcs to be account for block-filtering based on too low energy. If empty, do not use
-            ip.addOptional( 'fileFilterOut', {} ); % blocks containing these files get filtered out
-            ip.addOptional( 'sourcesMinEnergy', -20 ); 
+%             ip.addOptional( 'fileFilterOut', {} ); % blocks containing these files get filtered out
             ip.parse( varargin{:} );
-            obj = obj@LabelCreators.Base( ...
-                        'labelBlockSize_s', ip.Results.labelBlockSize_s, ...
-                        'removeUnclearBlocks', ip.Results.removeUnclearBlocks );
-            obj.maxNegBlockToEventRatio = ip.Results.maxNegBlockToEventRatio;
+            obj = obj@LabelCreators.TimeSeriesLabelCreator( 'removeUnclearBlocks', ...
+                                                         ip.Results.removeUnclearBlocks );
             obj.types = ip.Results.types;
             obj.negOut = ip.Results.negOut;
             obj.srcPrioMethod = ip.Results.srcPrioMethod;
-            obj.segIdTargetSrcFilter = ip.Results.segIdTargetSrcFilter;
+%             obj.segIdTargetSrcFilter = ip.Results.segIdTargetSrcFilter;
             obj.srcTypeFilterOut = ip.Results.srcTypeFilterOut;
-            obj.nrgSrcsFilter = ip.Results.nrgSrcsFilter;
-            obj.sourcesMinEnergy = ip.Results.sourcesMinEnergy;
-            obj.fileFilterOut = sort( ip.Results.fileFilterOut );
+%             obj.fileFilterOut = sort( ip.Results.fileFilterOut );
             obj.procName = [obj.procName '(' strcat( obj.types{1}{:} ) ')'];
         end
         %% -------------------------------------------------------------------------------
@@ -61,17 +49,13 @@ classdef MultiEventTypeTimeSeriesLabeler < LabelCreators.Base
     methods (Access = protected)
         
         function outputDeps = getLabelInternOutputDependencies( obj )
-            outputDeps.minBlockEventRatio = obj.minBlockToEventRatio;
-            outputDeps.maxNegBlockToEventRatio = obj.maxNegBlockToEventRatio;
             outputDeps.types = obj.types;
             outputDeps.negOut = obj.negOut;
             outputDeps.srcPrioMethod = obj.srcPrioMethod;
-            outputDeps.nrgSrcsFilter = obj.nrgSrcsFilter;
-            outputDeps.sourcesMinEnergy = obj.sourcesMinEnergy;
             outputDeps.srcTypeFilterOut = sortrows( obj.srcTypeFilterOut );
             outputDeps.segIdTargetSrcFilter = sortrows( obj.segIdTargetSrcFilter );
             outputDeps.fileFilterOut = obj.fileFilterOut;
-            outputDeps.v = 9;
+            outputDeps.v = 1;
         end
         %% -------------------------------------------------------------------------------
         
@@ -81,111 +65,95 @@ classdef MultiEventTypeTimeSeriesLabeler < LabelCreators.Base
         %% -------------------------------------------------------------------------------
         
         function [y, ysi] = label( obj, blockAnnotations )
-            [activeTypes, relBlockEventOverlap, srcIdxs] = obj.getActiveTypes( blockAnnotations );
-            [maxPosRelOverlap,maxTimeTypeIdx] = max( relBlockEventOverlap );
-            ysi = {};
-            if any( activeTypes )
+            [activeTypes, srcIdxs] = obj.getActiveTypes( blockAnnotations );
+            y = zeros( size( activeTypes, 1 ), 1 );
+            ysi = cell( size( activeTypes, 1 ), 1 );
+            if any( activeTypes(:) )
                 switch obj.srcPrioMethod
                     case 'energy'
-                        eSrcs = cellfun( @mean, blockAnnotations.srcEnergy(:,:) ); % mean over channels
-                        for ii = 1 : numel( activeTypes )
-                            if activeTypes(ii)
-                                eTypes(ii) = 1/sum( 1./eSrcs([srcIdxs{ii}]) );
-                            else
-                                eTypes(ii) = -inf;
-                            end
+                        error( 'AMLTTP:notImplemented', 'energy PrioMethod for time-series not implemented yet' );
+                        for ss = 1 : size( blockAnnotations.globalSrcEnergy.globalSrcEnergy, 2 )
+                            eSrcs(:,ss) = mean( cell2mat( ...
+                                blockAnnotations.globalSrcEnergy.globalSrcEnergy(:,ss) ), 2 );
                         end
-                        [~,labelTypeIdx] = max( eTypes );
+                        eTypes = eSrcs .* activeTypes;
+                        [~,labelTypeIdxs] = max( eTypes, [], 2 );
+                        y = labelTypeIdxs .* double( any( activeTypes, 2 ) );
                     case 'order'
-                        labelTypeIdx = find( activeTypes, 1, 'first' );
-                    case 'time'
-                        labelTypeIdx = maxTimeTypeIdx;
+                        labelTypeIdxs = activeTypes .* repmat( 1:size( activeTypes, 2), size( activeTypes, 1), 1 );
+                        y = min( labelTypeIdxs, [], 2 );
                     otherwise
                         error( 'AMLTTP:unknownOptionValue', ['%s: unknown option value.'...
                                      'Use ''energy'' or ''order''.'], obj.srcPrioMethod );
                 end
-                y = labelTypeIdx;
-                ysi = srcIdxs(y);
-            elseif strcmp( obj.negOut, 'rest' ) && ...
-                    (maxPosRelOverlap <= obj.maxNegBlockToEventRatio) 
-                y = -1;
+                ysi_ = y;
+                ysi_(ysi_==0) = 1;
+                ysi = srcIdxs(sub2ind( size( srcIdxs ), 1:size( srcIdxs, 1 ), ysi_' ));
+            end
+            if strcmp( obj.negOut, 'rest' )
+                y(y==0) = -1;
             else
-                y = NaN;
+                y(y==0) = NaN;
                 return;
             end
-            if ~isempty( obj.segIdTargetSrcFilter )
-                for ii = 1 : size( obj.segIdTargetSrcFilter, 1 )
-                    srcf = obj.segIdTargetSrcFilter(ii,1);
-                    typef = obj.segIdTargetSrcFilter(ii,2);
-                    srcfAzm = obj.lastConfig{obj.sceneId}.preceding.preceding.preceding.preceding.preceding.sceneCfg.sources(srcf).azimuth;
-                    if isa( srcfAzm, 'SceneConfig.ValGen' )
-                        srcfAzm = srcfAzm.val;
-                    end
-                    if activeTypes(typef) && any( abs( blockAnnotations.srcAzms(srcIdxs{typef}) - srcfAzm ) >= 0.1 )
-                        y = NaN;
-                        return;
-                    end
-                end
-            end
+%             if ~isempty( obj.segIdTargetSrcFilter )
+%                 for ii = 1 : size( obj.segIdTargetSrcFilter, 1 )
+%                     srcf = obj.segIdTargetSrcFilter(ii,1);
+%                     typef = obj.segIdTargetSrcFilter(ii,2);
+%                     srcfAzm = obj.lastConfig{obj.sceneId}.preceding.preceding.preceding.preceding.preceding.sceneCfg.sources(srcf).azimuth;
+%                     if isa( srcfAzm, 'SceneConfig.ValGen' )
+%                         srcfAzm = srcfAzm.val;
+%                     end
+%                     if activeTypes(typef) && any( abs( blockAnnotations.srcAzms(srcIdxs{typef}) - srcfAzm ) >= 0.1 )
+%                         y = NaN;
+%                         return;
+%                     end
+%                 end
+%             end
             for ii = 1 : size( obj.srcTypeFilterOut, 1 )
                 srcfo = obj.srcTypeFilterOut(ii,1);
                 typefo = obj.srcTypeFilterOut(ii,2);
-                if activeTypes(typefo) && any( srcIdxs{typefo} == srcfo )
-                    y = NaN;
-                    return;
-                end
+                fo_lidxs = activeTypes(:,typefo) ...
+                           & cellfun( @(si)(any( si == srcfo )), srcIdxs(:,typefo) );
+                y(fo_lidxs) = NaN;
             end
-            if ~isempty( obj.nrgSrcsFilter )
-                rejectBlock = LabelCreators.EnergyDependentLabeler.isEnergyTooLow( ...
-                              blockAnnotations, obj.nrgSrcsFilter, obj.sourcesMinEnergy );
-                if rejectBlock
-                    y = NaN;
-                    return;
-                end
-            end
-            for ii = 1 : numel( obj.fileFilterOut )
-                if any( strcmpi( obj.fileFilterOut{ii}, blockAnnotations.srcFile.srcFile(:,1) ) )
-                    y = NaN;
-                    return;
-                end
-            end
+%             for ii = 1 : numel( obj.fileFilterOut )
+%                 if any( strcmpi( obj.fileFilterOut{ii}, blockAnnotations.srcFile.srcFile(:,1) ) )
+%                     y = NaN;
+%                     return;
+%                 end
+%             end
         end
         %% -------------------------------------------------------------------------------
-        function [activeTypes, relBlockEventOverlap, srcIdxs] = getActiveTypes( obj, blockAnnotations )
-            [relBlockEventOverlap, srcIdxs] = obj.relBlockEventsOverlap( blockAnnotations );
-            activeTypes = relBlockEventOverlap >= obj.minBlockToEventRatio;
-        end
         
-        function [relBlockEventsOverlap, srcIdxs] = relBlockEventsOverlap( obj, blockAnnotations )
-            blockOffset = blockAnnotations.blockOffset;
-            labelBlockOnset = blockOffset - obj.labelBlockSize_s;
+        function [activeTypes, srcIdxs] = getActiveTypes( obj, blockAnnotations )
+            ts = blockAnnotations.globalSrcEnergy.t;
+            activeTypes = zeros( numel( ts ), numel( obj.types ) );
+            srcIdxs = cell( numel( ts ), numel( obj.types ) );
             eventOnsets = blockAnnotations.srcType.t.onset;
             eventOffsets = blockAnnotations.srcType.t.offset;
-            relBlockEventsOverlap = zeros( size( obj.types ) );
-            srcIdxs = cell( size( obj.types ) );
-            for ii = 1 : numel( obj.types )
-                eventsAreType = cellfun( @(ba)(...
-                                  obj.eventIsType( ii, ba )...
-                                              ), blockAnnotations.srcType.srcType(:,1) );
-                thisTypeEventOnOffs = ...
-                               [eventOnsets(eventsAreType)' eventOffsets(eventsAreType)'];
-                thisTypeMergedEventOnOffs = sortAndMergeOnOffs( thisTypeEventOnOffs );
-                thisTypeMergedOnsets = thisTypeMergedEventOnOffs(:,1);
-                thisTypeMergedOffsets = thisTypeMergedEventOnOffs(:,2);
-                eventBlockOverlaps = arrayfun( @(eon,eof)(...
-                                  min( blockOffset, eof ) - max( labelBlockOnset, eon )...
-                                         ), thisTypeMergedOnsets, thisTypeMergedOffsets );
-                isEventBlockOverlap = eventBlockOverlaps' > 0;
-                eventBlockOverlapLen = sum( eventBlockOverlaps(isEventBlockOverlap) );
-                if eventBlockOverlapLen == 0
-                    relBlockEventsOverlap(ii) = 0;
-                else
-                    eventLen = sum( thisTypeMergedOffsets(isEventBlockOverlap) ...
-                                            - thisTypeMergedOnsets(isEventBlockOverlap) );
-                    maxBlockEventLen = min( obj.labelBlockSize_s, eventLen );
-                    relBlockEventsOverlap(ii) = eventBlockOverlapLen / maxBlockEventLen;
+            for tt = 1 : numel( obj.types )
+                eventsAreType = cellfun( @(ba)(obj.eventIsType( tt, ba )), ...
+                                                  blockAnnotations.srcType.srcType(:,1) );
+                srcIdxs_tt = [blockAnnotations.srcType.srcType{eventsAreType,2}];
+                eventOnOffs_tt = [eventOnsets(eventsAreType)',eventOffsets(eventsAreType)'];
+                eventOnOffs_tt = eventOnOffs_tt - ts(1) + 1;
+                for ii = 1:2
+                    eventOnOffs_tt(:,ii) = max( ...
+                                                [zeros( size(eventOnOffs_tt, 1), 1 ), ...
+                                                 eventOnOffs_tt(:,ii)], [], 2 );
+                    eventOnOffs_tt(:,ii) = min( ...
+                                   [repmat( numel( ts ), size(eventOnOffs_tt, 1), 1 ), ...
+                                    eventOnOffs_tt(:,ii)], [], 2 );
                 end
-                srcIdxs{ii} = unique( [blockAnnotations.srcType.srcType{eventsAreType,2}] );
+                for jj = 1 : size( eventOnOffs_tt, 1 )
+                    event_jj_idxs = eventOnOffs_tt(jj,1) : eventOnOffs_tt(jj,2);
+                    activeTypes(event_jj_idxs,tt) = 1;
+                    srcIdxs(event_jj_idxs,tt) = cellfun( @(a,b)([a,b]), ...
+                                srcIdxs(event_jj_idxs,tt), ...
+                                repmat( {srcIdxs_tt(jj)}, numel( event_jj_idxs ), 1 ), ...
+                                                                 'UniformOutput', false );
+                end
             end
         end
         %% -------------------------------------------------------------------------------

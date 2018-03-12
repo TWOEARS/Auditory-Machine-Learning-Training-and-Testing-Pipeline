895943a66880655e12fe8428af105591d51f3738
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-12 23:34:19 +0100

    added alpha select to GlmNetLambdaSelectTrainer.m; removed useless GlmNetModelSelectTrainer.m

--- src/+ModelTrainers/GlmNetLambdaSelectTrainer.m
+++ src/+ModelTrainers/GlmNetLambdaSelectTrainer.m
@@ -10,7 +10,7 @@ classdef GlmNetLambdaSelectTrainer < ModelTrainers.Base & Parameterized
         cvTrainer;      % for k-fold cross validation
         coreTrainer;
         fullSetModel;
-        alpha;          % elastic net parameter
+        alphas;          % elastic net parameter
         family;
         nLambda;        % number of lambdas on the regularization path
         hpsMaxDataSize;
@@ -21,9 +21,9 @@ classdef GlmNetLambdaSelectTrainer < ModelTrainers.Base & Parameterized
     methods
 
         function obj = GlmNetLambdaSelectTrainer( varargin )
-            pds{1} = struct( 'name', 'alpha', ...
-                             'default', 1, ...
-                             'valFun', @(x)(isfloat(x) && x >= 0 && x <= 1.0) );
+            pds{1} = struct( 'name', 'alphas', ...
+                             'default', [0 0.5 0.99], ...
+                             'valFun', @(x)(all(isfloat(x) & x>=0 & x<=1.0)) );
             pds{2} = struct( 'name', 'family', ...
                              'default', 'binomial', ...
                              'valFun', @(x)(ischar(x) && any(strcmpi(x, ...
@@ -44,6 +44,7 @@ classdef GlmNetLambdaSelectTrainer < ModelTrainers.Base & Parameterized
             obj = obj@Parameterized( pds );
             obj = obj@ModelTrainers.Base( varargin{:} );
             obj.setParameters( true, varargin{:} );
+            obj.alphas = unique( obj.alphas );
         end
         %% -------------------------------------------------------------------------------
         
@@ -53,44 +54,48 @@ classdef GlmNetLambdaSelectTrainer < ModelTrainers.Base & Parameterized
         %% ----------------------------------------------------------------
         
         function buildModel( obj, ~, ~, ~ )
-            verboseFprintf( obj, '\nRun on full trainSet...\n' );
-            obj.coreTrainer = ModelTrainers.GlmNetTrainer( ...
-                'performanceMeasure', obj.performanceMeasure, ...
-                'maxDataSize', obj.maxDataSize, ...
-                'maxTestDataSize', obj.maxTestDataSize, ...
-                'dataSelector', obj.dataSelector, ...
-                'alpha', obj.alpha, ...
-                'family', obj.family, ...
-                'nLambda', obj.nLambda, ...
-                'importanceWeighter', obj.importanceWeighter );
-            obj.coreTrainer.setData( obj.trainSet, obj.testSet );
-            obj.coreTrainer.run();
-            obj.fullSetModel = obj.coreTrainer.getModel();
-            lambdas = obj.fullSetModel.model.lambda;
-            verboseFprintf( obj, '\nRun cv to determine best lambda...\n' );
-            obj.coreTrainer.setParameters( false, 'lambda', lambdas,...
-                                                  'maxDataSize', obj.hpsMaxDataSize );
-            obj.cvTrainer = ModelTrainers.CVtrainer( obj.coreTrainer );
-            obj.cvTrainer.setPerformanceMeasure( obj.performanceMeasure );
-            obj.cvTrainer.setData( obj.trainSet, [] );
-            obj.cvTrainer.setNumberOfFolds( obj.cvFolds );
-            obj.cvTrainer.run();
-            cvModels = obj.cvTrainer.models;
-            verboseFprintf( obj, 'Calculate Performance for all lambdas...\n' );
-            lPerfs = zeros( numel( lambdas ), numel( cvModels ) );
-            for ii = 1 : numel( cvModels )
-                cvModels{ii}.setLambda( [] );
-                foldPerfs = Models.Base.getPerformance( ...
-                    cvModels{ii}, obj.cvTrainer.folds{ii}, obj.performanceMeasure, ...
-                    obj.maxDataSize, obj.dataSelector, obj.importanceWeighter, false );
-                lPerfs(1:numel(foldPerfs),ii) = foldPerfs;
-                verboseFprintf( obj, '.' );
+            for aa = 1 : numel( obj.alphas )
+                alpha = obj.alphas(aa);
+                verboseFprintf( obj, '\n\nRunning alpha=%f...\n', alpha );
+                verboseFprintf( obj, '\n1.) Full trainSet...\n' );
+                obj.coreTrainer = ModelTrainers.GlmNetTrainer( ...
+                    'performanceMeasure', obj.performanceMeasure, ...
+                    'maxDataSize', obj.maxDataSize, ...
+                    'maxTestDataSize', obj.maxTestDataSize, ...
+                    'dataSelector', obj.dataSelector, ...
+                    'alpha', alpha, ...
+                    'family', obj.family, ...
+                    'nLambda', obj.nLambda, ...
+                    'importanceWeighter', obj.importanceWeighter );
+                obj.coreTrainer.setData( obj.trainSet, obj.testSet );
+                obj.coreTrainer.run();
+                obj.fullSetModel{aa} = obj.coreTrainer.getModel();
+                lambdas = obj.fullSetModel{aa}.model.lambda;
+                verboseFprintf( obj, '\n2.) CV for determining best lambda...\n' );
+                obj.coreTrainer.setParameters( false, 'lambda', lambdas,...
+                    'maxDataSize', obj.hpsMaxDataSize );
+                obj.cvTrainer = ModelTrainers.CVtrainer( obj.coreTrainer );
+                obj.cvTrainer.setPerformanceMeasure( obj.performanceMeasure );
+                obj.cvTrainer.setData( obj.trainSet, [] );
+                obj.cvTrainer.setNumberOfFolds( obj.cvFolds );
+                obj.cvTrainer.run();
+                cvModels = obj.cvTrainer.models;
+                verboseFprintf( obj, 'Calculate Performance for all lambdas...\n' );
+                lPerfs = zeros( numel( lambdas ), numel( cvModels ) );
+                for ii = 1 : numel( cvModels )
+                    cvModels{ii}.setLambda( [] );
+                    foldPerfs = Models.Base.getPerformance( ...
+                        cvModels{ii}, obj.cvTrainer.folds{ii}, obj.performanceMeasure, ...
+                        obj.maxDataSize, obj.dataSelector, obj.importanceWeighter, false );
+                    lPerfs(1:numel(foldPerfs),ii) = foldPerfs;
+                    verboseFprintf( obj, '.' );
+                end
+                obj.fullSetModel{aa}.lPerfsMean = nanMean( lPerfs, 2 );
+                obj.fullSetModel{aa}.lPerfsStd = nanStd( lPerfs, 2 );
+                verboseFprintf( obj, 'Done\n' );
+                lambdasSortedByPerf = sortrows( [lambdas,obj.fullSetModel{aa}.lPerfsMean], 2 );
+                obj.fullSetModel{aa}.setLambda( lambdasSortedByPerf(end,1) );
             end
-            obj.fullSetModel.lPerfsMean = nanMean( lPerfs, 2 );
-            obj.fullSetModel.lPerfsStd = nanStd( lPerfs, 2 );
-            verboseFprintf( obj, 'Done\n' );
-            lambdasSortedByPerf = sortrows( [lambdas,obj.fullSetModel.lPerfsMean], 2 );
-            obj.fullSetModel.setLambda( lambdasSortedByPerf(end,1) );
         end
         %% -------------------------------------------------------------------------------
 
@@ -100,7 +105,12 @@ classdef GlmNetLambdaSelectTrainer < ModelTrainers.Base & Parameterized
     methods (Access = protected)
         
         function model = giveTrainedModel( obj )
-            model = obj.fullSetModel;
+            alphaPerfs = zeros( size( obj.fullSetModel ) );
+            for aa = 1 : numel( obj.fullSetModel )
+                alphaPerfs(aa) = max( obj.fullSetModel{aa}.lPerfsMean );
+            end
+            [~,max_aa] = max( alphaPerfs );
+            model = obj.fullSetModel{max_aa};
         end
         %% -------------------------------------------------------------------------------
         

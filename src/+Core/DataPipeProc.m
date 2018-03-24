ce6bfd48f42f28c0ca5645b95f1122bf5e5f7a26
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-24 22:41:33 +0100

    get rid of a few memReports

--- src/+Core/DataPipeProc.m
+++ src/+Core/DataPipeProc.m
@@ -173,7 +173,7 @@ classdef DataPipeProc < handle
                 end
                 dfii = dfii + 1;
                 fprintf( '\n' );
-                freemem = memReport() % for debugging
+%                 freemem = memReport() % for debugging
                 fprintf( '\n' );
             end
             obj.dataFileProcessor.saveCacheDirectory();

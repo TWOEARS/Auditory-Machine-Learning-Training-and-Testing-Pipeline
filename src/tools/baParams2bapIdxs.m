feea3046ad88e5dc9df08f32b800afef33b24d6a
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-25 16:45:27 +0200

    baParams2bapIdxs.m: looped version without cells might be faster

--- src/tools/baParams2bapIdxs.m
+++ src/tools/baParams2bapIdxs.m
@@ -1,54 +1,33 @@
 function baParamIdxs = baParams2bapIdxs( baParams )
 
 emptyBapi = nanRescStruct;
+emptyBapi = rmfield( emptyBapi, 'estAzm' );
 baParamIdxs = repmat( emptyBapi, numel( baParams ), 1);
 
-tmp = num2cell( nan2inf( [baParams.classIdx] ) );
-[baParamIdxs.classIdx] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.nAct] + 1 ) );
-[baParamIdxs.nAct] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curSnr]+35)/5 ) + 1 ) );
-[baParamIdxs.curSnr] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curSnr_db]+35)/5 ) + 1 ) );
-[baParamIdxs.curSnr_db] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curSnr2]+35)/5 ) + 1 ) );
-[baParamIdxs.curSnr2] = tmp{:};
-tmp = num2cell( nan2inf( round( [baParams.azmErr]/5 ) + 1 ) );
-[baParamIdxs.azmErr] = tmp{:};
-tmp = num2cell( nan2inf( round( [baParams.azmErr2]/5 ) + 1 ) );
-[baParamIdxs.azmErr2] = tmp{:};
-tmp = num2cell( nan2inf( round( (wrapTo180([baParams.gtAzm])+180)/5 ) + 1 ) );
-[baParamIdxs.gtAzm] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.nStream] + 1 ) );
-[baParamIdxs.nStream] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.nAct_segStream] + 1 ) );
-[baParamIdxs.nAct_segStream] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curNrj]+35)/5 ) + 1 ) );
-[baParamIdxs.curNrj] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curNrj_db]+35)/5 ) + 1 ) );
-[baParamIdxs.curNrj_db] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curNrjOthers]+35)/5 ) + 1 ) );
-[baParamIdxs.curNrjOthers] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.curNrjOthers_db]+35)/5 ) + 1 ) );
-[baParamIdxs.curNrjOthers_db] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.scpId] ) );
-[baParamIdxs.scpId] = tmp{:};
-tmp = num2cell( nan2inf( max( 1, [baParams.scpId] - 255 + 1 ) ) );
-[baParamIdxs.scpIdExt] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.fileId] ) );
-[baParamIdxs.fileId] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.fileClassId] ) );
-[baParamIdxs.fileClassId] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.posPresent] + 1 ) );
-[baParamIdxs.posPresent] = tmp{:};
-tmp = num2cell( nan2inf( round( ([baParams.posSnr]+35)/5 ) + 1 ) );
-[baParamIdxs.posSnr] = tmp{:};
-tmp = num2cell( nan2inf( [baParams.blockClass] ) );
-[baParamIdxs.blockClass] = tmp{:};
-tmp = num2cell( nan2inf( ([baParams.dist2bisector]+1)*10 + 1 ) );
-[baParamIdxs.dist2bisector] = tmp{:};
-
-baParamIdxs = rmfield( baParamIdxs, 'estAzm' );
+for ii = 1 : numel( baParams )
+baParamIdxs(ii).classIdx =        nan2inf( baParams(ii).classIdx );
+baParamIdxs(ii).nAct =            nan2inf( baParams(ii).nAct + 1 );
+baParamIdxs(ii).curSnr =          nan2inf( round( (baParams(ii).curSnr+35)/5 ) + 1 );
+baParamIdxs(ii).curSnr_db =       nan2inf( round( (baParams(ii).curSnr_db+35)/5 ) + 1 );
+baParamIdxs(ii).curSnr2 =         nan2inf( round( (baParams(ii).curSnr2+35)/5 ) + 1 );
+baParamIdxs(ii).azmErr =          nan2inf( round( baParams(ii).azmErr/5 ) + 1 );
+baParamIdxs(ii).azmErr2 =         nan2inf( round( baParams(ii).azmErr2/5 ) + 1 );
+baParamIdxs(ii).gtAzm =           nan2inf( round( (wrapTo180(baParams(ii).gtAzm)+180)/5 ) + 1 );
+baParamIdxs(ii).nStream =         nan2inf( baParams(ii).nStream + 1 );
+baParamIdxs(ii).nAct_segStream =  nan2inf( baParams(ii).nAct_segStream + 1 );
+baParamIdxs(ii).curNrj =          nan2inf( round( (baParams(ii).curNrj+35)/5 ) + 1 );
+baParamIdxs(ii).curNrj_db =       nan2inf( round( (baParams(ii).curNrj_db+35)/5 ) + 1 );
+baParamIdxs(ii).curNrjOthers =    nan2inf( round( (baParams(ii).curNrjOthers+35)/5 ) + 1 );
+baParamIdxs(ii).curNrjOthers_db = nan2inf( round( (baParams(ii).curNrjOthers_db+35)/5 ) + 1 );
+baParamIdxs(ii).scpId =           nan2inf( baParams(ii).scpId );
+baParamIdxs(ii).scpIdExt =        nan2inf( max( 1, baParams(ii).scpId - 255 + 1 ) );
+baParamIdxs(ii).fileId =          nan2inf( baParams(ii).fileId );
+baParamIdxs(ii).fileClassId =     nan2inf( baParams(ii).fileClassId );
+baParamIdxs(ii).posPresent =      nan2inf( baParams(ii).posPresent + 1 );
+baParamIdxs(ii).posSnr =          nan2inf( round( (baParams(ii).posSnr+35)/5 ) + 1 );
+baParamIdxs(ii).blockClass =      nan2inf( baParams(ii).blockClass );
+baParamIdxs(ii).dist2bisector =   nan2inf( (baParams(ii).dist2bisector+1)*10 + 1 );
+end
 
 end
 

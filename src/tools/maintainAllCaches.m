fa5c92539b8bcc1e587b5109586bf5c0845434c1
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-25 15:50:45 +0200

    maintainAllCaches.m: add options

--- src/tools/maintainAllCaches.m
+++ src/tools/maintainAllCaches.m
@@ -1,7 +1,36 @@
-function maintainAllCaches( idPipeCacheDir )
+function maintainAllCaches( idPipeCacheDir, chooseDirs, parM )
+
+if nargin < 2 || isempty( chooseDirs ), chooseDirs = false; end
+if nargin < 3 || isempty( parM ), parM = 12; end
 
 ipcDirs = dir( idPipeCacheDir );
-parfor (jj = 1 : numel( ipcDirs ), 6)
+
+if chooseDirs
+    for jj = numel( ipcDirs ):-1:1
+
+        if ~ipcDirs(jj).isdir
+            ipcDirs(jj) = []; 
+            continue;
+        end
+        if ipcDirs(jj).name(1) == '.'
+            ipcDirs(jj) = []; 
+            continue;
+        end
+        cacheDir_jj = [idPipeCacheDir filesep ipcDirs(jj).name];
+        if all( arrayfun( @(a)(a.name(1)), dir( cacheDir_jj ) ) == '.' )
+            ipcDirs(jj) = []; 
+            continue;
+        end
+        
+        choice = input( sprintf( 'Maintain %s? [y/n]', ipcDirs(jj).name ), 's' );
+        if choice == 'n'
+            ipcDirs(jj) = [];
+            continue;
+        end
+    end
+end
+
+parfor (jj = 1 : numel( ipcDirs ), parM)
 
     if ~ipcDirs(jj).isdir, continue; end
     if ipcDirs(jj).name(1) == '.', continue; end

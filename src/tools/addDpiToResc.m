7e8896d6bd022d8e680c13da17d4e21e11e52580
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-25 17:09:19 +0200

    addDpiToResc.m potential speedup

--- src/tools/addDpiToResc.m
+++ src/tools/addDpiToResc.m
@@ -31,7 +31,7 @@ for ii = 2 : numel( bapiFields )
 end
 
 [C,~,ic] = unique( C, 'rows' );
-paramFactor = arrayfun( @(x)(sum( x == ic )), 1:size( C, 1 ) );
+paramFactor = accumarray( ic, ones( size( C, 1 ), 1 ) );
 resc = resc.addData( C, paramFactor', true );
 
 end

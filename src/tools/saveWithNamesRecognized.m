1f82bd4b7e386ae66879a3e658e74779c2ffb6df
ivo trowitzsch <ivo.trowitzsch@gmail.com>
2018-03-25 13:23:56 +0200

    bugfix saveWithNamesRecognized.m

--- src/tools/saveWithNamesRecognized.m
+++ src/tools/saveWithNamesRecognized.m
@@ -4,6 +4,6 @@ if ~isstruct( allVarsInStruct )
     error( 'Usage:StructNeeded', '''allVarsInStruct'' needs to be a struct containing the variables to be saved.' );
 end
 
-save( filepath, '-struct', allVarsInStruct, varargin{:} );
+save( filepath, '-struct', 'allVarsInStruct', varargin{:} );
 
 end
